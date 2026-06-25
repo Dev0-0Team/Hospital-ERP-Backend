c530948f8ab7728b47e62a6067ae2f59f75b6be6
Compiler-A <alimoussajob@gmail.com>
2026-06-25 14:08:20 +0300

    feature(delete): add all class releative this implementation

---
+++ Hospital-ERP-Backend.Application/Persons/Commands/DeletePerson/DeletePersonValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using Hospital_ERP_Backend.Domain.Entities;
+
+namespace Hospital_ERP_Backend.Application.Persons.Commands.DeletePerson
+{
+    public class DeletePersonValidator : AbstractValidator<DeletePersonRequest>
+    {
+        public DeletePersonValidator() 
+        {
+            RuleFor(x => x.Id).GreaterThanOrEqualTo(1);
+        }
+    }
+}
