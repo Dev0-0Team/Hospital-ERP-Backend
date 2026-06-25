443285759fe055d6ef8a269e8c19568727c9d377
Compiler-A <alimoussajob@gmail.com>
2026-06-25 12:15:02 +0300

    feature(get): add all classes connect with this implementation

---
+++ Hospital-ERP-Backend.Application/Persons/Queries/GetPerson/GetPersonService.cs
@@ -0,0 +1,46 @@
+﻿using FluentValidation;
+using Hospital_ERP_Backend.Application.Persons.Queries.GetAllPersons;
+using Hospital_ERP_Backend.Domain.Entities;
+using Hospital_ERP_Backend.Domain.Interfaces.Base;
+
+
+namespace Hospital_ERP_Backend.Application.Persons.Queries.GetPerson
+{
+    public class GetPersonService
+    {
+        private readonly IValidator<GetPersonRequest> _validator;
+        private readonly IBaseQueryRepository<Person> _iPerson;
+
+        public GetPersonService(IValidator<GetPersonRequest> validator, IBaseQueryRepository<Person> iPerson)
+        {
+            _validator = validator;
+            _iPerson = iPerson;
+        }
+
+        public async Task<GetPersonResponse> GetPersonAsync(GetPersonRequest request)
+        {
+            var validationResult = await _validator.ValidateAsync(request);
+            if (!validationResult.IsValid)
+            {
+                throw new ValidationException(validationResult.Errors);
+            }
+            var person = await _iPerson.GetAsync(request.Id);
+
+            if (person == null)
+            {
+                throw new KeyNotFoundException("Not Found!");
+            }
+
+            return new GetPersonResponse
+            {
+                Id = person.Id,
+                FullName = person.FullName,
+                Dob = person.Dob,
+                Gender = person.Gender,
+                Phone = person.Phone,
+                Address = person.Address
+            };
+        }
+
+    }
+}
---
+++ Hospital-ERP-Backend.Application/Persons/Queries/GetPerson/GetPersonValidator.cs
@@ -0,0 +1,13 @@
+﻿
+using FluentValidation;
+
+namespace Hospital_ERP_Backend.Application.Persons.Queries.GetPerson
+{
+    public class GetPersonValidator : AbstractValidator<GetPersonRequest>
+    {
+        public GetPersonValidator()
+        {
+            RuleFor(x => x.Id).GreaterThanOrEqualTo(1);
+        }
+    }
+}
