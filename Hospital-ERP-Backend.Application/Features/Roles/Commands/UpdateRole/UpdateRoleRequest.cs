5576be4df04e886a4d4e65b9352ce9fc60adc52d
Compiler-A <alimoussajob@gmail.com>
2026-06-27 09:36:28 +0300

    feature(role): add update operator in application layer

---
+++ Hospital-ERP-Backend.Application/Features/Roles/Commands/UpdateRole/UpdateRoleRequest.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace Hospital_ERP_Backend.Application.Features.Roles.Commands.UpdateRole
+{
+    public record UpdateRoleRequest
+    {
+        public int Id { get; init; }
+        public string Name { get; init; } = null!;
+    }
+}
