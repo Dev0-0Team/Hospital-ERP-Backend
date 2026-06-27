d7899ad205913b65c84844c9fb376e16a3b4a450
Compiler-A <alimoussajob@gmail.com>
2026-06-27 09:00:15 +0300

    feature(role): add get all operator in application layer

---
+++ Hospital-ERP-Backend.Application/Features/Roles/Queries/GetAllRoles/GetAllRolesResponse.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace Hospital_ERP_Backend.Application.Features.Roles.Queries.GetAllRoles
+{
+    public record GetAllRolesResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
---
+++ Hospital-ERP-Backend.Application/Features/Roles/Queries/GetAllRoles/GetAllRolesValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Hospital_ERP_Backend.Application.Features.Roles.Queries.GetAllRoles
+{
+    public class GetAllRolesValidator : AbstractValidator<GetAllRolesRequest>
+    {
+        public GetAllRolesValidator()
+        {
+            RuleFor(x => x.Page)
+                .GreaterThanOrEqualTo(1)
+                .WithMessage("Page number must be a positive integer.");
+        }
+    }
+}
