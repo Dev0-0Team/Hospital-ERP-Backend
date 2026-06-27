33e3fe77711c26d41288f4933bb1ee7ced25e0ae
Compiler-A <alimoussajob@gmail.com>
2026-06-27 10:18:05 +0300

    feature(role): add delete operator in application layer

---
+++ Hospital-ERP-Backend.Application/Features/Roles/Commands/DeleteRole/DeleteRoleRequest.cs
@@ -0,0 +1,9 @@
+﻿
+
+namespace Hospital_ERP_Backend.Application.Features.Roles.Commands.DeleteRole
+{
+    public record DeleteRoleRequest
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Hospital-ERP-Backend.Application/Features/Roles/Commands/DeleteRole/DeleteRoleService.cs
@@ -0,0 +1,40 @@
+﻿
+
+using FluentValidation;
+using Hospital_ERP_Backend.Domain.Entities;
+using Hospital_ERP_Backend.Domain.Interfaces.Base;
+
+namespace Hospital_ERP_Backend.Application.Features.Roles.Commands.DeleteRole
+{
+    public class DeleteRoleService
+    {
+        private readonly IValidator<DeleteRoleRequest> _validator;
+        private readonly IBaseCommandRepository<Role> _iRole;
+        private readonly IBaseQueryRepository<Role> _iRoleQuery;
+
+        public DeleteRoleService(IValidator<DeleteRoleRequest> validator, IBaseCommandRepository<Role> iRole, IBaseQueryRepository<Role> iRoleQuery)
+        {
+            _validator = validator;
+            _iRole = iRole;
+            _iRoleQuery = iRoleQuery;
+        }
+
+        public async Task<bool> DeleteRoleAsync(DeleteRoleRequest request)
+        {
+            var validationResult = await _validator.ValidateAsync(request);
+            if (!validationResult.IsValid)
+            {
+                throw new ArgumentException($"Invalid request: {string.Join(", ", validationResult.Errors.Select(e => e.ErrorMessage))}");
+            }
+
+            var role = await _iRoleQuery.GetAsync(request.Id);
+            if (role == null)
+            {
+                throw new KeyNotFoundException($"Role with Id {request.Id} not found.");
+            }
+            var isDeleted = await _iRole.DeleteAsync(role.Id);
+            return isDeleted;
+        }>
+
+    }
+}
