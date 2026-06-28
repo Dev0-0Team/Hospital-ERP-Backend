73ed946fad151d47e2adca6325e60666311ad84d
Compiler-A <alimoussajob@gmail.com>
2026-06-28 13:02:13 +0300

    feature(permission): add create implementation

---
+++ Hospital-ERP-Backend.Application/Features/Permissions/Commands/CreatePermission/CreatePermissionResponse.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace Hospital_ERP_Backend.Application.Features.Permissions.Commands.CreatePermission
+{
+    public record CreatePermissionResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
