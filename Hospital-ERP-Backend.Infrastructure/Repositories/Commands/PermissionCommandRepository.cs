7c75e7df5d86a0b5af7906652fc7b8af37018666
Compiler-A <alimoussajob@gmail.com>
2026-06-28 09:25:46 +0300

    feature(permission): add command repository

---
+++ Hospital-ERP-Backend.Infrastructure/Repositories/Commands/PermissionCommandRepository.cs
@@ -0,0 +1,49 @@
+﻿
+using Hospital_ERP_Backend.Domain.Entities;
+using Hospital_ERP_Backend.Domain.Interfaces.Base;
+using Hospital_ERP_Backend.Infrastructure.Data;
+
+namespace Hospital_ERP_Backend.Infrastructure.Repositories.Commands
+{
+    public class PermissionCommandRepository : IBaseCommandRepository<Permission> 
+    {
+        private readonly HospitalDbContext _dbContext;
+        
+        public PermissionCommandRepository(HospitalDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Permission?> CreateAsync(Permission entity)
+        {
+            Permission permission = entity;
+
+            await _dbContext.AddAsync(permission);
+            await _dbContext.SaveChangesAsync();
+            return permission;
+        }
+
+        public async Task<Permission?> UpdateAsync(Permission entity)
+        {
+            Permission? permission = await _dbContext.Permissions.FindAsync(entity.Id);
+            if (permission == null)
+                return null;
+
+            _dbContext.Entry(permission).CurrentValues.SetValues(entity);
+            await _dbContext.SaveChangesAsync();
+            return permission;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            Permission? permission = await _dbContext.Permissions.FindAsync(id);
+            if (permission == null)
+            {
+                return false;
+            }
+
+            _dbContext.Remove(permission);
+            return await _dbContext.SaveChangesAsync() > 0;
+        }
+    }
+}
