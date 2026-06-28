ef0b3551a5ef751ec8f61dedfb9190b37eca0945
Compiler-A <alimoussajob@gmail.com>
2026-06-28 09:45:02 +0300

    feature(permission): add query repository

---
+++ Hospital-ERP-Backend.Infrastructure/Repositories/Queries/PermissionQueryRepository.cs
@@ -0,0 +1,52 @@
+﻿using Dapper;
+using Hospital_ERP_Backend.Domain.Entities;
+using Hospital_ERP_Backend.Domain.Interfaces.Base;
+using Hospital_ERP_Backend.Infrastructure.Data;
+using Hospital_ERP_Backend.Infrastructure.Setting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.Data;
+
+namespace Hospital_ERP_Backend.Infrastructure.Repositories.Queries
+{
+    public class PermissionQueryRepository : IBaseQueryRepository<Permission>
+    {
+        private readonly IDbConnection _connection;
+        private readonly MySetting _setting;
+
+        public PermissionQueryRepository(HospitalDbContext hospitalDbContext, IOptions<MySetting> setting)
+        {
+            _connection = hospitalDbContext.Database.GetDbConnection();
+            _setting = setting.Value;
+        }
+
+        public async Task<Permission?> GetAsync(int id)
+        {
+            var parameters = new
+            {
+                id = id
+            };
+            var query = "permissions.SP_GetPermissionById";
+            return await _connection.QueryFirstOrDefaultAsync<Permission>(
+                query,
+                parameters,
+                commandType: CommandType.StoredProcedure
+            );
+        }
+
+        public async Task<IEnumerable<Permission>> GetAllAsync(int page)
+        {
+            var parameters = new
+            {
+                page,
+                rows = _setting.RowsPerPage
+            };
+            var query = "permissions.SP_GetAllPermissions";
+            return await _connection.QueryAsync<Permission>(
+                query,
+                parameters,
+                commandType: CommandType.StoredProcedure
+            );
+        }
+    }
+}
