311fee12be41bec2916c5f9c307d59028b73e5fc
Compiler-A <alimoussajob@gmail.com>
2026-06-28 12:51:46 +0300

    feature(permission): add get implementation

---
+++ Hospital-ERP-Backend.Application/Features/Permissions/Queries/GetPermission/GetPermissionResponse.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Hospital_ERP_Backend.Application.Features.Permissions.Queries.GetPermission
+{
+    public class GetPermissionResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
