0e265e40102f94a8665ed2429010fde6463baae2
Compiler-A <alimoussajob@gmail.com>
2026-06-30 14:49:39 +0300

    feature(user): delete using name space not used in user services extensions

--- Hospital-ERP-Backend.Application/Features/Users/Extensions/UserServiceExtensions.cs
+++ Hospital-ERP-Backend.Application/Features/Users/Extensions/UserServiceExtensions.cs
@@ -1,16 +1,10 @@
 ﻿using FluentValidation;
-using Hospital_ERP_Backend.Application.Features.Persons.Commands.CreatePerson;
-using Hospital_ERP_Backend.Application.Features.Persons.Commands.DeletePerson;
-using Hospital_ERP_Backend.Application.Features.Persons.Commands.UpdatePerson;
-using Hospital_ERP_Backend.Application.Features.Persons.Queries.GetAllPersons;
-using Hospital_ERP_Backend.Application.Features.Persons.Queries.GetPerson;
 using Hospital_ERP_Backend.Application.Features.Users.Commands.CreateUser;
 using Hospital_ERP_Backend.Application.Features.Users.Commands.DeleteUser;
 using Hospital_ERP_Backend.Application.Features.Users.Commands.UpdateUser;
 using Hospital_ERP_Backend.Application.Features.Users.Queries.GetAllUsers;
 using Hospital_ERP_Backend.Application.Features.Users.Queries.GetUser;
 using Microsoft.Extensions.DependencyInjection;
-using System;
 
 
 namespace Hospital_ERP_Backend.Application.Features.Users.Extensions
