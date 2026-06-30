2b82422b580fd0b1f39aee31aaace8eab727bbfe
Compiler-A <alimoussajob@gmail.com>
2026-06-30 11:46:41 +0300

    feature(user): add query repository

---
+++ Hospital-ERP-Backend.Infrastructure/Repositories/Queries/UserQueryRepository.cs
@@ -0,0 +1,52 @@
+﻿using Dapper;
+using Hospital_ERP_Backend.Domain.Entities;
+using Hospital_ERP_Backend.Domain.Interfaces.Base;
+using Hospital_ERP_Backend.Infrastructure.Data;
+using Hospital_ERP_Backend.Infrastructure.Setting;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+
+namespace Hospital_ERP_Backend.Infrastructure.Repositories.Queries
+{
+    public class UserQueryRepository : IBaseQueryRepository<User>
+    {
+        private readonly MySetting _setting;
+        private readonly IDbConnection _connection;
+
+        public UserQueryRepository(MySetting setting, HospitalDbContext hospitalDbContext)
+        {
+            _setting = setting;
+            _connection = hospitalDbContext.Database.GetDbConnection();
+        }
+
+        public async Task<User?> GetAsync(int id)
+        {
+            var parameters = new
+            {
+                id = id
+            };
+            var query = "users.SP_GetUserById";
+            return await _connection.QueryFirstOrDefaultAsync<User>(
+                query,
+                parameters,
+                commandType: CommandType.StoredProcedure
+            );
+        }
+
+        public async Task<IEnumerable<User>> GetAllAsync(int page)
+        {
+            var parameters = new
+            {
+                page,
+                rows = _setting.RowsPerPage
+            };
+            var query = "users.SP_GetAllUsers";
+            return await _connection.QueryAsync<User>(
+                query,
+                parameters,
+                commandType: CommandType.StoredProcedure
+            );
+        }
+    }
+}
