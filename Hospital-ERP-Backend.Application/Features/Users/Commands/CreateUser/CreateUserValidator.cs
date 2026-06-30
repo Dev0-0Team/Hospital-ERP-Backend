47b9eeb050a3cc57bb8edbee815f2b96499d428c
Compiler-A <alimoussajob@gmail.com>
2026-06-30 14:22:20 +0300

    feature(user): add create user

---
+++ Hospital-ERP-Backend.Application/Features/Users/Commands/CreateUser/CreateUserValidator.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+
+namespace Hospital_ERP_Backend.Application.Features.Users.Commands.CreateUser
+{
+    public class CreateUserValidator : AbstractValidator<CreateUserRequest>
+    {
+        public CreateUserValidator()
+        {
+            RuleFor(x => x.PersonId).GreaterThan(0)
+                .WithMessage("Id must be greater than 0.");
+            
+            RuleFor(x => x.Email)
+                .MaximumLength(255)
+                .When(x => x.Email != null)
+                .WithMessage("Email must be at most 255 characters long");
+
+            RuleFor(x => x.Password)
+                .MaximumLength(char.MaxValue)
+                .When(x => x.Password != null)
+                .WithMessage($"Password must be at most {char.MaxValue} characters long");
+
+            RuleFor(x => x.Status)
+                .NotEmpty()
+                .Must(BeValidStatus)
+                .WithMessage("Status must be Active or InActive");
+        }
+
+        private bool BeValidStatus(string Status)
+        {
+            var allowed = new[] { "Active", "InActive"};
+            return allowed.Contains(Status);
+        }
+    }
+}
