d98f651282c44383ff9e6dd91e93de1f7879c294
Compiler-A <alimoussajob@gmail.com>
2026-07-02 14:26:23 +0300

    feature(user role): add create user role in application layer

---
+++ Hospital-ERP-Backend.Application/Features/UserRoles/Commands/CreateUserRole/CreateUserRoleRequest.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Hospital_ERP_Backend.Application.Features.UserRoles.Commands.CreateUserRole
+{
+    public record CreateUserRoleRequest
+    {
+        public int UserId { get; set; }
+        public int RoleId { get; set; }
+    }
+}
