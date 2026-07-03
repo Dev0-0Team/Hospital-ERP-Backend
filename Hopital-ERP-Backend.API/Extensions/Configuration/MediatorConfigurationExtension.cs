ac467f2f578791a26107dbc1c98aaa0c1d77f254
Compiler-A <alimoussajob@gmail.com>
2026-07-03 17:39:09 +0300

    feature(mediator): add mediator configuration in program.cs

---
+++ Hopital-ERP-Backend.API/Extensions/Configuration/MediatorConfigurationExtension.cs
@@ -0,0 +1,14 @@
+﻿namespace Hopital_ERP_Backend.API.Extensions.Configuration
+{
+    public static class MediatorConfigurationExtension
+    {
+        public static IServiceCollection AddMediatorConfigurationExtension(this IServiceCollection services)
+        {
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(typeof(Program).Assembly);
+            });
+            return services;
+        }
+    }
+}
