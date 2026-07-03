b5cc4524021d9b12891d8f120a11eb106c33db16
Compiler-A <alimoussajob@gmail.com>
2026-07-03 10:52:24 +0300

    feature(role permission): add update role permission operator

---
+++ Hospital-ERP-Backend.Application/Features/RolePermissions/Command/UpdateRolePermission/UpdateRolePermissionService.cs
@@ -0,0 +1,68 @@
+﻿using FluentValidation;
+using Hospital_ERP_Backend.Domain.Entities;
+using Hospital_ERP_Backend.Domain.Interfaces.Base;
+
+
+namespace Hospital_ERP_Backend.Application.Features.RolePermissions.Command.UpdateRolePermission
+{
+    public class UpdateRolePermissionService
+    {
+        private readonly IBaseCommandRepository<RolePermission> _iRolePermission;
+        private readonly IBaseQueryRepository<RolePermission> _iRolePermissionQuery;
+        private readonly IBaseQueryRepository<Role> _iRoleQuery;
+        private readonly IBaseQueryRepository<Permission> _iPermissionQuery;
+        private readonly IValidator<UpdateRolePermissionRequest> _iValidator;
+
+        public UpdateRolePermissionService(
+            IBaseCommandRepository<RolePermission> iRolePermission,
+            IBaseQueryRepository<Role> iRoleQuery,
+            IBaseQueryRepository<Permission> iPermissionQuery,
+            IValidator<UpdateRolePermissionRequest> iValidator,
+            IBaseQueryRepository<RolePermission> iRolePermissionQuery)
+        {
+            _iRolePermission = iRolePermission;
+            _iRoleQuery = iRoleQuery;
+            _iPermissionQuery = iPermissionQuery;
+            _iValidator = iValidator;
+            _iRolePermissionQuery = iRolePermissionQuery;
+        }
+
+        public async Task<UpdateRolePermissionResponse> UpdateRolePermissionAsync(UpdateRolePermissionRequest request)
+        {
+            var validationResult = await _iValidator.ValidateAsync(request);
+            if (!validationResult.IsValid)
+            {
+                throw new ValidationException(validationResult.Errors);
+            }
+            RolePermission? rolePermission = await _iRolePermissionQuery.GetAsync(request.Id);
+            if (rolePermission == null)
+            {
+                throw new KeyNotFoundException($"RolePermission with Id {request.Id} not found.");
+            }
+            var role = await _iRoleQuery.GetAsync(request.RoleId);
+            if (role == null)
+            {
+                throw new KeyNotFoundException($"Role with Id {request.RoleId} not found.");
+            }
+            var permission = await _iPermissionQuery.GetAsync(request.PermissionId);
+            if (permission == null)
+            {
+                throw new KeyNotFoundException($"Permission with Id {request.PermissionId} not found.");
+            }
+            rolePermission.RoleId = request.RoleId;
+            rolePermission.PermissionId = request.PermissionId;
+
+            RolePermission? result = await _iRolePermission.UpdateAsync(rolePermission);
+            if (result == null)
+            {
+                throw new InvalidOperationException("Failed to update Role Permission.");
+            }
+            return new UpdateRolePermissionResponse
+            {
+                Id = result.Id,
+                RoleId = result.RoleId,
+                PermissionId = result.PermissionId
+            };
+        }
+    }
+}
---
+++ Hospital-ERP-Backend.Application/Features/RolePermissions/Command/UpdateRolePermission/UpdateRolePermissionValidator.cs
@@ -0,0 +1,19 @@
+﻿
+
+using FluentValidation;
+
+namespace Hospital_ERP_Backend.Application.Features.RolePermissions.Command.UpdateRolePermission
+{
+    public class UpdateRolePermissionValidator : AbstractValidator<UpdateRolePermissionRequest>
+    {
+        public UpdateRolePermissionValidator()
+        {
+            RuleFor(x => x.Id)
+                .GreaterThan(0).WithMessage("Id must be greater than 0.");
+            RuleFor(x => x.RoleId)
+                .GreaterThan(0).WithMessage("RoleId must be greater than 0.");
+            RuleFor(x => x.PermissionId)
+                .GreaterThan(0).WithMessage("PermissionId must be greater than 0.");
+        }
+    }
+}
