38d2ddca7d11b7b75deff2b2663ec5b3b6907605
Compiler-A <alimoussajob@gmail.com>
2026-07-03 10:41:24 +0300

    feature(role permission): add create role permission operator

---
+++ Hospital-ERP-Backend.Application/Features/RolePermissions/Command/CreateRolePermission/CreateRolePermissionValidator.cs
@@ -0,0 +1,16 @@
+﻿
+using FluentValidation;
+
+namespace Hospital_ERP_Backend.Application.Features.RolePermissions.Command.CreateRolePermission
+{
+    public class CreateRolePermissionValidator : AbstractValidator<CreateRolePermissionRequest>
+    {
+        public CreateRolePermissionValidator()
+        {
+            RuleFor(x => x.RoleId)
+                .GreaterThan(0).WithMessage("RoleId must be greater than 0.");
+            RuleFor(x => x.PermissionId)
+                .GreaterThan(0).WithMessage("PermissionId must be greater than 0.");
+        }
+    }
+}
