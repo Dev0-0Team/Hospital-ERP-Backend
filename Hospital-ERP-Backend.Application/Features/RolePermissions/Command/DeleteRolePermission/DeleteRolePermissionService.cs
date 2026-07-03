2e6ee5fa3ef13cd2a3b0e99b7f50b2a0641621ac
Compiler-A <alimoussajob@gmail.com>
2026-07-03 10:17:20 +0300

    feature(role permission): add delete role permission operator

---
+++ Hospital-ERP-Backend.Application/Features/RolePermissions/Command/DeleteRolePermission/DeleteRolePermissionService.cs
@@ -0,0 +1,42 @@
+﻿
+
+using FluentValidation;
+using Hospital_ERP_Backend.Domain.Entities;
+using Hospital_ERP_Backend.Domain.Interfaces.Base;
+
+namespace Hospital_ERP_Backend.Application.Features.RolePermissions.Command.DeleteRolePermission
+{
+    public class DeleteRolePermissionService
+    {
+        private readonly IBaseCommandRepository<RolePermission> _iRolePermission;
+        private readonly IBaseQueryRepository<RolePermission> _iRolePermissionQuery;
+        private readonly IValidator<DeleteRolePermissionRequest> _iValidator;
+
+        public DeleteRolePermissionService(IBaseCommandRepository<RolePermission> iRolePermission, IBaseQueryRepository<RolePermission> iRolePermissionQuery, IValidator<DeleteRolePermissionRequest> iValidator)
+        {
+            _iRolePermission = iRolePermission;
+            _iRolePermissionQuery = iRolePermissionQuery;
+            _iValidator = iValidator;
+        }
+
+        public async Task<bool> DeleteRolePermissionAsync(DeleteRolePermissionRequest request)
+        {
+            var validationResult = await _iValidator.ValidateAsync(request);
+            if (!validationResult.IsValid)
+            {
+                throw new ArgumentException($"Invalid request: {string.Join(", ", validationResult.Errors.Select(e => e.ErrorMessage))}");
+            }
+            RolePermission? rolePermission = await _iRolePermissionQuery.GetAsync(request.Id);
+            if (rolePermission == null)
+            {
+                throw new KeyNotFoundException($"Role Permission with Id {request.Id} not found.");
+            }
+            bool isDeleted = await _iRolePermission.DeleteAsync(rolePermission.Id);
+            if (!isDeleted)
+            {
+                throw new InvalidOperationException($"Failed to delete role permission with Id {request.Id}.");
+            }
+            return isDeleted;
+        }
+    }
+}
---
+++ Hospital-ERP-Backend.Application/Features/RolePermissions/Command/DeleteRolePermission/DeleteRolePermissionValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Hospital_ERP_Backend.Application.Features.RolePermissions.Command.DeleteRolePermission
+{
+    public class DeleteRolePermissionValidator : AbstractValidator<DeleteRolePermissionRequest>
+    {
+        public DeleteRolePermissionValidator()
+        {
+            RuleFor(x => x.Id)
+                .GreaterThan(0).WithMessage("Id must be greater than 0.");
+        }
+    }
+}
+
