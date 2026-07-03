ec5dc09392e761735e9dd2fa4f0777cd874db79a
Compiler-A <alimoussajob@gmail.com>
2026-07-03 09:40:12 +0300

    feature(role permission): add get all role permission operator

---
+++ Hospital-ERP-Backend.Application/Features/RolePermissions/Query/GetAllRolePermissions/GetAllRolePermissionsService.cs
@@ -0,0 +1,40 @@
+﻿
+
+using FluentValidation;
+using Hospital_ERP_Backend.Domain.Entities;
+using Hospital_ERP_Backend.Domain.Interfaces.Base;
+
+namespace Hospital_ERP_Backend.Application.Features.RolePermissions.Query.GetAllRolePermissions
+{
+    public class GetAllRolePermissionsService
+    {
+        private readonly IBaseQueryRepository<RolePermission> _iRolePermission;
+        private readonly IValidator<GetAllRolePermissionsRequest> _validator;
+
+        public GetAllRolePermissionsService(IValidator<GetAllRolePermissionsRequest> validator, IBaseQueryRepository<RolePermission> iRolePermission)
+        {
+            _validator = validator;
+            _iRolePermission = iRolePermission;
+        }
+
+        public async Task<IEnumerable<GetAllRolePermissionsResponse>> GetAllRolePermissionsAsync(GetAllRolePermissionsRequest request)
+        {
+            var validationResult = await _validator.ValidateAsync(request);
+            if (!validationResult.IsValid)
+            {
+                throw new ArgumentException($"Invalid request: {string.Join(", ", validationResult.Errors.Select(e => e.ErrorMessage))}");
+            }
+            var rolePermissions = await _iRolePermission.GetAllAsync(request.Page);
+            if (rolePermissions == null || rolePermissions.Count() == 0)
+            {
+                throw new KeyNotFoundException($"No role permissions found on page {request.Page}.");
+            }
+            return rolePermissions.Select(rp => new GetAllRolePermissionsResponse
+            {
+                Id = rp.Id,
+                RoleId = rp.RoleId,
+                PermissionId = rp.PermissionId
+            });
+        }
+    }
+}
