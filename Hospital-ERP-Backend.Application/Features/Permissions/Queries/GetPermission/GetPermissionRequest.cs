1f4b7c39de14ed928ca9d10b502698bb88268d53
Compiler-A <alimoussajob@gmail.com>
2026-07-03 18:56:56 +0300

    feature(mediator): add mediator for permission

--- Hospital-ERP-Backend.Application/Features/Permissions/Queries/GetPermission/GetPermissionRequest.cs
+++ Hospital-ERP-Backend.Application/Features/Permissions/Queries/GetPermission/GetPermissionRequest.cs
@@ -1,8 +1,10 @@
 ﻿
 
+using MediatR;
+
 namespace Hospital_ERP_Backend.Application.Features.Permissions.Queries.GetPermission
 {
-    public record GetPermissionRequest
+    public record GetPermissionRequest : IRequest<GetPermissionResponse>
     {
         public int Id { get; set; }
     }
