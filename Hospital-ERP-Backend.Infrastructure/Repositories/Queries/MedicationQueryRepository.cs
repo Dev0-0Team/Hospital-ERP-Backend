0104dfdcdd2dbc2b9233217a34542b343f5621d6
Abdelrahman Owais <abdoo.owais@gmail.com>
2026-07-05 21:07:04 +0300

    feat(medication): add medication query repository

--- Hospital-ERP-Backend.Infrastructure/Repositories/Queries/MedicationQueryRepository.cs
+++ Hospital-ERP-Backend.Infrastructure/Repositories/Queries/MedicationQueryRepository.cs
@@ -1,12 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Dapper;
+using Hospital_ERP_Backend.Domain.Entities;
+using Hospital_ERP_Backend.Domain.Interfaces.Base;
+using Hospital_ERP_Backend.Infrastructure.Data;
+using Hospital_ERP_Backend.Infrastructure.Setting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.Data;
 
-namespace Hospital_ERP_Backend.Infrastructure.Repositories.Queries
+public class MedicationQueryRepository
+    : IBaseQueryRepository<Medication>
 {
-    internal class MedicationQueryRepository
+    private readonly IDbConnection _connection;
+    private readonly MySetting _setting;
+
+    public MedicationQueryRepository(HospitalDbContext hospitalDbContext, IOptions<MySetting> setting)
     {
+        _connection = hospitalDbContext.Database.GetDbConnection();
+        _setting = setting.Value;
+    }
+
+    public async Task<Medication?> GetAsync(int id)
+    {
+        var parameters = new
+        {
+            id
+        };
+
+        return await _connection.QueryFirstOrDefaultAsync<Medication>(
+            "medication.SP_GetMedicationById", parameters, commandType: CommandType.StoredProcedure);
+    }
+
+    public async Task<IEnumerable<Medication>> GetAllAsync(int page)
+    {
+        var parameters = new
+        {
+            page,
+            rows = _setting.RowsPerPage
+        };
+
+        return await _connection.QueryAsync<Medication>("medication.SP_GetAllMedications",
+            parameters, commandType: CommandType.StoredProcedure);
     }
 }
