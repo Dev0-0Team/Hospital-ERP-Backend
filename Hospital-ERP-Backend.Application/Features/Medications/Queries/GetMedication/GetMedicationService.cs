b639fdd5647b6fc622154cb8f13f2db9be8a8366
Abdelrahman Owais <abdoo.owais@gmail.com>
2026-07-06 02:25:12 +0300

    feat(medications): implement get medication query

---
+++ Hospital-ERP-Backend.Application/Features/Medications/Queries/GetMedication/GetMedicationService.cs
@@ -0,0 +1,47 @@
+﻿using FluentValidation;
+using Hospital_ERP_Backend.Application.Features.Medications.Queries.GetAllMedications;
+using Hospital_ERP_Backend.Domain.Entities;
+using Hospital_ERP_Backend.Domain.Interfaces.Base;
+using MediatR;
+
+namespace Hospital_ERP_Backend.Application.Features.Medications.Queries.GetMedicationById
+{
+    public class GetMedicationService : IRequestHandler<GetMedicationRequest, GetMedicationResponse>
+    {
+        private readonly IValidator<GetMedicationRequest> _validator;
+        private readonly IBaseQueryRepository<Medication> _medication;
+
+        public GetMedicationService(IValidator<GetMedicationRequest> validator, IBaseQueryRepository<Medication> medication)
+        {
+            _validator = validator;
+            _medication = medication;
+        }
+        public Task<GetMedicationResponse> Handle(GetMedicationRequest request, CancellationToken cancellationToken)
+        {
+            return GetMedicationAsync(request);
+        }
+        private async Task<GetMedicationResponse> GetMedicationAsync(GetMedicationRequest request)
+        {
+            var validationResult = await _validator.ValidateAsync(request);
+
+            if (!validationResult.IsValid)
+            {
+                throw new ArgumentException($"Invalid request: {string.Join(", ", validationResult.Errors.Select(e => e.ErrorMessage))}");
+            }
+
+            var medication = await _medication.GetAsync(request.Id);
+            if (medication == null)
+            {
+                throw new KeyNotFoundException($"No medication found with ID {request.Id}.");
+            }
+            return new GetMedicationResponse
+            {
+                Id = medication.Id,
+                Name = medication.Name,
+                DosageForm = medication.DosageForm,
+                Manufacturer = medication.Manufacturer ?? string.Empty
+            };
+        }
+
+    }
+}
---
+++ Hospital-ERP-Backend.Application/Features/Medications/Queries/GetMedication/GetMedicationValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Hospital_ERP_Backend.Application.Features.Medications.Queries.GetMedicationById
+{
+    public class GetMedicationValidator : AbstractValidator<GetMedicationRequest>
+    {
+        public GetMedicationValidator()
+        {
+            RuleFor(x => x.Id)
+                .GreaterThan(0).WithMessage("Medication ID must be greater than 0.");
+        }
+    }
+}
