7591f5ce3ad0354d10a19b12a77364b8b893098b
Abdelrahman Owais <abdoo.owais@gmail.com>
2026-07-06 14:40:35 +0300

    feat(medication): implement soft delete medication command

---
+++ Hospital-ERP-Backend.Application/Features/Medications/Commands/DeleteMedication/DeleteMedicationRequest.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Hospital_ERP_Backend.Application.Features.Medications.Commands.DeleteMedication
+{
+    public class DeleteMedicationRequest : IRequest<bool>
+    {
+        public int Id { get; set; }
+
+    }
+}
