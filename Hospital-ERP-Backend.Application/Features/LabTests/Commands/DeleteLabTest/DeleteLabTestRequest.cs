e9c03e6f967640badbd428e9b5773fad64cc5bb5
Abdelrahman Owais <abdoo.owais@gmail.com>
2026-07-08 19:27:12 +0300

    feat(lab test): Add soft delete lab test command

---
+++ Hospital-ERP-Backend.Application/Features/LabTests/Commands/DeleteLabTest/DeleteLabTestRequest.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Hospital_ERP_Backend.Application.Features.LabTests.Commands.DeleteLabTest
+{
+    public record DeleteLabTestRequest() : IRequest<bool>
+    {
+        public int Id { get; set; }
+    }
+}
