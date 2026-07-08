e2f338dc41cb07c5e40c49ca0dd4bb919c9ff47b
Abdelrahman Owais <abdoo.owais@gmail.com>
2026-07-08 17:31:53 +0300

    feat(lab test): Add get lab test by id query

---
+++ Hospital-ERP-Backend.Application/Features/LabTests/Queries/GetLabTest/GetLabTestRequest.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Hospital_ERP_Backend.Application.Features.LabTests.Queries.GetLabTest
+{
+    public class GetLabTestRequest : IRequest<GetLabTestResponse>
+    {
+        public int Id { get; set; }
+
+    }
+}
