c574a73f88367185b672426ce2270fe26b386a3a
ahmed <ahmedayman25606@gmail.com>
2026-07-09 18:29:01 +0200

    Register Queue Priority Application services in DI

--- Hospital-ERP-Backend.Application/Features/QueuePriorities/Extensions/QueuePriorityServiceExtensions.cs
+++ Hospital-ERP-Backend.Application/Features/QueuePriorities/Extensions/QueuePriorityServiceExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using Hospital_ERP_Backend.Application.Features.QueuePriorities.Commands.CreateQueuePriority;
+using Hospital_ERP_Backend.Application.Features.QueuePriorities.Commands.DeleteQueuePriority;
 using Hospital_ERP_Backend.Application.Features.QueuePriorities.Commands.UpdateQueuePriority;
 using Hospital_ERP_Backend.Application.Features.QueuePriorities.Queries.GetAllQueuePriorities;
 using Hospital_ERP_Backend.Application.Features.QueuePriorities.Queries.GetQueuePriority;
@@ -9,18 +10,20 @@ namespace Hospital_ERP_Backend.Application.Features.QueuePriorities.Extensions
 {
     public static class QueuePriorityServiceExtensions
     {
-       
+     
         public static IServiceCollection AddQueuePriorityServicesExtension(this IServiceCollection Services)
         {
             Services.AddScoped<IValidator<GetAllQueuePrioritiesRequest>, GetAllQueuePrioritiesValidator>();
-            Services.AddScoped<GetAllQueuePrioritiesService>();
             Services.AddScoped<IValidator<GetQueuePriorityRequest>, GetQueuePriorityValidator>();
-            Services.AddScoped<GetQueuePriorityService>();
             Services.AddScoped<IValidator<CreateQueuePriorityRequest>, CreateQueuePriorityValidator>();
-            Services.AddScoped<CreateQueuePriorityService>();
             Services.AddScoped<IValidator<UpdateQueuePriorityRequest>, UpdateQueuePriorityValidator>();
-            Services.AddScoped<UpdateQueuePriorityService>();
+            Services.AddScoped<IValidator<DeleteQueuePriorityRequest>, DeleteQueuePriorityValidator>();
 
+            Services.AddScoped<GetAllQueuePrioritiesService>();
+            Services.AddScoped<GetQueuePriorityService>();
+            Services.AddScoped<CreateQueuePriorityService>();
+            Services.AddScoped<UpdateQueuePriorityService>();
+            Services.AddScoped<DeleteQueuePriorityService>();
 
             return Services;
         }
