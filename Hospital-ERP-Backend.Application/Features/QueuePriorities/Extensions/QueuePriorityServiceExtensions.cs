e75447a8e79c766405643ae496fe37b81d8e5470
ahmed <ahmedayman25606@gmail.com>
2026-07-09 18:12:24 +0200

    Get All Queue Priorities Application Layer

---
+++ Hospital-ERP-Backend.Application/Features/QueuePriorities/Extensions/QueuePriorityServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Hospital_ERP_Backend.Application.Features.QueuePriorities.Queries.GetAllQueuePriorities;
+using Hospital_ERP_Backend.Application.Features.QueuePriorities.Quries.GetAllQueuePriorities;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hospital_ERP_Backend.Application.Features.QueuePriorities.Extensions
+{
+    public static class QueuePriorityServiceExtensions
+    {
+       
+        public static IServiceCollection AddQueuePriorityServicesExtension(this IServiceCollection Services)
+        {
+            Services.AddScoped<IValidator<GetAllQueuePrioritiesRequest>, GetAllQueuePrioritiesValidator>();
+            Services.AddScoped<GetAllQueuePrioritiesService>();
+
+            return Services;
+        }
+    }
+}
