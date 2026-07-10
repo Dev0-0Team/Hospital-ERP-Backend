293e81018c68c88627420dc6fc801dcd2d501184
Abdelrahman Owais <abdoo.owais@gmail.com>
2026-07-10 16:52:28 +0300

    feat(medication-inventory): Add UpdateMedicationInventory command

---
+++ Hospital-ERP-Backend.Application/Features/MedicationInventories/Commands/UpdateMedicationInventory/UpdateMedicationInventoryRequest.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace Hospital_ERP_Backend.Application.Features.MedicationInventories.Commands.UpdateMedicationInventory
+{
+    public record UpdateMedicationInventoryRequest : IRequest<UpdateMedicationInventoryResponse>
+    {
+        public int Id { get; set; }
+
+        public int MedicationId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public DateOnly ExpiryDate { get; set; }
+    }
+}
