3e972318ed5f62f003320e8efa30f6f99553ce67
Compiler-A <alimoussajob@gmail.com>
2026-07-11 11:36:23 +0300

    refactor(queue priority): inheritance base query repository

--- Hospital-ERP-Backend.Infrastructure/Repositories/Queries/QueuePriorityQueryRepository.cs
+++ Hospital-ERP-Backend.Infrastructure/Repositories/Queries/QueuePriorityQueryRepository.cs
@@ -2,6 +2,7 @@
 using Hospital_ERP_Backend.Domain.Entities;
 using Hospital_ERP_Backend.Domain.Interfaces.Base;
 using Hospital_ERP_Backend.Infrastructure.Data;
+using Hospital_ERP_Backend.Infrastructure.Repositories.Queries.Base;
 using Hospital_ERP_Backend.Infrastructure.Setting;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
@@ -10,36 +11,12 @@ using System.Data;
 
 namespace Hospital_ERP_Backend.Infrastructure.Repositories.Queries
 {
-    public class QueuePriorityQueryRepository : IBaseQueryRepository<QueuePriority>, IDisposable
+    public class QueuePriorityQueryRepository : BaseQueryRepository<QueuePriority>
     {
-        private readonly MySetting _setting;
-        private readonly IDbConnection _connection;
+        protected override string GetAllSpName => "queue_priorities.SP_GetAllQueuePriorities";
+        protected override string GetByIdSpName => "queue_priorities.SP_GetQueuePriorityById";
 
-        public QueuePriorityQueryRepository(IOptions<MySetting> setting)
-        {
-            _setting = setting.Value;
-            _connection = new SqlConnection(_setting.ConnectionString);
-        }
+        public QueuePriorityQueryRepository(IOptions<MySetting> setting) : base(setting) { }
 
-        public async Task<QueuePriority?> GetAsync(int id)
-        {
-            var parameters = new { id = id };
-            var query = "queue_priorities.SP_GetQueuePriorityById";
-            return await _connection.QueryFirstOrDefaultAsync<QueuePriority>(
-                query, parameters, commandType: CommandType.StoredProcedure);
-        }
-
-        public async Task<IEnumerable<QueuePriority>> GetAllAsync(int page)
-        {
-            var parameters = new { page, rows = _setting.RowsPerPage };
-            var query = "queue_priorities.SP_GetAllQueuePriorities";
-            return await _connection.QueryAsync<QueuePriority>(
-                query, parameters, commandType: CommandType.StoredProcedure);
-        }
-
-        public void Dispose()
-        {
-            _connection?.Dispose();
-        }
     }
 }
