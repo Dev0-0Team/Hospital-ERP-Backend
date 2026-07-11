3a935149163fbc135a1a3788a5fa0bbe99e991d2
Abdelrahman Owais <abdoo.owais@gmail.com>
2026-07-11 12:49:34 +0300

    feat(drug interaction): Add DeleteDrugInteraction command

---
+++ Hospital-ERP-Backend.Application/Features/DrugInteractions/Commands/DeleteDrugInteraction/DeleteDrugInteractionRequest.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Hospital_ERP_Backend.Application.Features.DrugInteractions.Commands.DeleteDrugInteraction
+{
+    public record DeleteDrugInteractionRequest : IRequest<bool>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Hospital-ERP-Backend.Application/Features/DrugInteractions/Commands/DeleteDrugInteraction/DeleteDrugInteractionValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Hospital_ERP_Backend.Application.Features.DrugInteractions.Commands.DeleteDrugInteraction
+{
+    public class DeleteDrugInteractionValidator : AbstractValidator<DeleteDrugInteractionRequest>
+    {
+        public DeleteDrugInteractionValidator()
+        {
+            RuleFor(x => x.Id)
+                .GreaterThan(0)
+                .WithMessage("Drug Interaction Id must be greater than 0.");
+        }
+    }
+}
