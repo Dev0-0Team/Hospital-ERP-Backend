622422746ebb8e88bc8fbf944d2e23d908f38e63
Compiler-A <alimoussajob@gmail.com>
2026-07-11 13:04:50 +0300

    refactor(queue priority): inheritance base command repository

--- Hospital-ERP-Backend.Infrastructure/Repositories/Commands/QueuePriorityCommandRepository.cs
+++ Hospital-ERP-Backend.Infrastructure/Repositories/Commands/QueuePriorityCommandRepository.cs
@@ -1,43 +1,13 @@
 ﻿using Hospital_ERP_Backend.Domain.Entities;
 using Hospital_ERP_Backend.Domain.Interfaces.Base;
 using Hospital_ERP_Backend.Infrastructure.Data;
+using Hospital_ERP_Backend.Infrastructure.Repositories.Commands.Base;
 
 namespace Hospital_ERP_Backend.Infrastructure.Repositories.Commands
 {
-    public class QueuePriorityCommandRepository : IBaseCommandRepository<QueuePriority>
+    public class QueuePriorityCommandRepository : BaseCommandRepository<QueuePriority>
     {
-        private readonly HospitalDbContext _dbContext;
-        public QueuePriorityCommandRepository(HospitalDbContext dbContext)
-        {
-            _dbContext = dbContext;
-        }
-        public async Task<QueuePriority?> CreateAsync(QueuePriority entity)
-        {
-            var queuePriority = await _dbContext.QueuePriorities.AddAsync(entity);
-            await _dbContext.SaveChangesAsync();
-            return queuePriority.Entity;
-        }
+        public QueuePriorityCommandRepository(HospitalDbContext dbContext) : base(dbContext) { }
 
-        public async Task<bool> DeleteAsync(int id)
-        {
-            QueuePriority? queuePriority = await _dbContext.QueuePriorities.FindAsync(id);
-            if (queuePriority == null) return false;
-
-            queuePriority.IsDeleted = true;
-            queuePriority.DeletedAt = DateTime.UtcNow;
-
-            return await _dbContext.SaveChangesAsync() > 0;
-        }
-
-        public async Task<QueuePriority?> UpdateAsync(QueuePriority entity)
-        {
-            QueuePriority? queuePriority = await _dbContext.QueuePriorities.FindAsync(entity.Id);
-            if (queuePriority == null) return null;
-
-            _dbContext.Entry(queuePriority).CurrentValues.SetValues(entity);
-
-            await _dbContext.SaveChangesAsync();
-            return queuePriority;
-        }
     }
 }
