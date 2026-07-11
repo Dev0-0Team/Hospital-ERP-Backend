8d160922a4c541adc5eabd46cda8fb936ef74138
Compiler-A <alimoussajob@gmail.com>
2026-07-11 13:04:08 +0300

    refactor(permission): inheritance base command repository

--- Hospital-ERP-Backend.Infrastructure/Repositories/Commands/PermissionCommandRepository.cs
+++ Hospital-ERP-Backend.Infrastructure/Repositories/Commands/PermissionCommandRepository.cs
@@ -7,45 +7,8 @@ namespace Hospital_ERP_Backend.Infrastructure.Repositories.Commands
 {
     public class PermissionCommandRepository : IBaseCommandRepository<Permission> 
     {
-        private readonly HospitalDbContext _dbContext;
-        
-        public PermissionCommandRepository(HospitalDbContext dbContext)
-        {
-            _dbContext = dbContext;
-        }
 
-        public async Task<Permission?> CreateAsync(Permission entity)
-        {
-            Permission permission = entity;
+        public PermissionCommandRepository(HospitalDbContext dbContext) : base(dbContext) { }
 
-            await _dbContext.Permissions.AddAsync(permission);
-            await _dbContext.SaveChangesAsync();
-            return permission;
-        }
-
-        public async Task<Permission?> UpdateAsync(Permission entity)
-        {
-            Permission? permission = await _dbContext.Permissions.FindAsync(entity.Id);
-            if (permission == null)
-                return null;
-
-            _dbContext.Entry(permission).CurrentValues.SetValues(entity);
-            await _dbContext.SaveChangesAsync();
-            return permission;
-        }
-
-        public async Task<bool> DeleteAsync(int id)
-        {
-            Permission? permission = await _dbContext.Permissions.FindAsync(id);
-            if (permission == null)
-            {
-                return false;
-            }
-
-            permission.IsDeleted = true;
-            permission.DeletedAt = DateTime.Now;
-
-            return await _dbContext.SaveChangesAsync() > 0;
-        }
     }
 }
