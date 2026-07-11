e9f8bc68a43123d93e1fe55bbb9f251c71c095f6
ahmed <ahmedayman25606@gmail.com>
2026-07-11 17:27:57 +0200

    Implement Get All Rooms in Application Layer

---
+++ Hospital-ERP-Backend.Application/Features/Rooms/Extensions/RoomServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using Hospital_ERP_Backend.Application.Features.Rooms.Queries.GetAllRooms;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hospital_ERP_Backend.Application.Features.Rooms.Extensions
+{
+    public static class RoomServiceExtensions
+    {
+        public static IServiceCollection AddRoomServicesExtension(this IServiceCollection Services)
+        {
+            Services.AddScoped<IValidator<GetAllRoomsRequest>, GetAllRoomsValidator>();
+
+            Services.AddScoped<GetAllRoomsService>();
+            return Services;
+        }
+    }
+}
