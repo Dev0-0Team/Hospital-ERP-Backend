6f33dea26073d6df2137e6313beee47c1d3a53e5
Abdelrahman Owais <abdoo.owais@gmail.com>
2026-07-11 13:27:32 +0300

    feat(drug interaction): Add DrugInteraction command repo

---
+++ Hospital-ERP-Backend.Infrastructure/Repositories/Commands/DrugInteractionCommandRepository.cs
@@ -0,0 +1,11 @@
+﻿using Hospital_ERP_Backend.Domain.Entities;
+using Hospital_ERP_Backend.Domain.Interfaces.Base;
+
+namespace Hospital_ERP_Backend.Infrastructure.Repositories.Commands
+{
+    public class DrugInteractionCommandRepository : IBaseCommandRepository<DrugInteraction>
+    {
+
+
+    }
+}
