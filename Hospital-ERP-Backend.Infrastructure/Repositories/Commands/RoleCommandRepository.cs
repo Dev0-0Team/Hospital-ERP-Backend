f3848f8b3349a1e4fb049ef976ad97d389182639
Compiler-A <alimoussajob@gmail.com>
2026-07-11 13:05:17 +0300

    refactor(role): inheritance base command repository

--- Hospital-ERP-Backend.Infrastructure/Repositories/Commands/RoleCommandRepository.cs
+++ Hospital-ERP-Backend.Infrastructure/Repositories/Commands/RoleCommandRepository.cs
@@ -3,50 +3,15 @@
 using Hospital_ERP_Backend.Domain.Entities;
 using Hospital_ERP_Backend.Domain.Interfaces.Base;
 using Hospital_ERP_Backend.Infrastructure.Data;
+using Hospital_ERP_Backend.Infrastructure.Repositories.Commands.Base;
 
 namespace Hospital_ERP_Backend.Infrastructure.Repositories.Commands
 {
-    public class RoleCommandRepository : IBaseCommandRepository<Role>
+    public class RoleCommandRepository : BaseCommandRepository<Role>
     {
-        private readonly HospitalDbContext _dbContext;
 
-        public RoleCommandRepository(HospitalDbContext dbContext)
-        {
-            _dbContext = dbContext;
-        }
+        public RoleCommandRepository(HospitalDbContext dbContext) : base(dbContext) { }
 
-        public async Task<Role?> CreateAsync(Role entity)
-        {
-            Role role = entity;
 
-            await _dbContext.Roles.AddAsync(role);
-            await _dbContext.SaveChangesAsync();
-            return role;
-        }
-
-        public async Task<Role?> UpdateAsync(Role entity)
-        {
-            Role? role = await _dbContext.Roles.FindAsync(entity.Id);
-            if (role == null)
-                return null;
-
-            _dbContext.Entry(role).CurrentValues.SetValues(entity);
-            await _dbContext.SaveChangesAsync();
-            return role;
-        }
-
-        public async Task<bool> DeleteAsync(int id)
-        {
-            Role? role = await _dbContext.Roles.FindAsync(id);
-            if (role == null)
-            {
-                return false;
-            }
-
-            role.IsDeleted = true;
-            role.DeletedAt = DateTime.Now;
-
-            return await _dbContext.SaveChangesAsync() > 0;
-        }
     }
 }
