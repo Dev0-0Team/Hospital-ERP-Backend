8adc002e8eb4980b55bc1893f66ef19310871f47
Compiler-A <alimoussajob@gmail.com>
2026-07-11 10:31:05 +0300

    refactor(base repository): add base command and query repository

---
+++ Hospital-ERP-Backend.Infrastructure/Repositories/Commands/Base/BaseCommandRepository.cs
@@ -0,0 +1,55 @@
+﻿using Hospital_ERP_Backend.Domain.Entities;
+using Hospital_ERP_Backend.Domain.Interfaces.Base;
+using Hospital_ERP_Backend.Infrastructure.Data;
+
+
+namespace Hospital_ERP_Backend.Infrastructure.Repositories.Commands.Base
+{
+    public class BaseCommandRepository<T> : IBaseCommandRepository<T> where T : BaseEntity
+    {
+
+        protected readonly HospitalDbContext _dbContext;
+        protected BaseCommandRepository(HospitalDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        public virtual async Task<T?> CreateAsync(T entity)
+        {
+            T newEntity = entity;
+
+            await _dbContext.Set<T>().AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+
+            return newEntity;
+        }
+
+        public virtual async Task<T?> UpdateAsync(T entity)
+        {
+            T? updatedEntity = await _dbContext.Set<T>().FindAsync(entity.Id);
+            if (updatedEntity == null)
+            {
+                return null;
+            }
+
+            _dbContext.Entry(updatedEntity).CurrentValues.SetValues(entity);
+            await _dbContext.SaveChangesAsync();
+
+            return updatedEntity;
+        }
+
+        public virtual async Task<bool> DeleteAsync(int id)
+        {
+            T? deleteEntity = await _dbContext.Set<T>().FindAsync(id);
+            if (deleteEntity == null)
+            {
+                return false;
+            }
+            deleteEntity.IsDeleted = true;
+            deleteEntity.DeletedAt = DateTime.Now;
+
+            return await _dbContext.SaveChangesAsync() > 0;
+        }
+    }
+}
---
+++ Hospital-ERP-Backend.Infrastructure/Repositories/Queries/Base/BaseQueryRepository.cs
@@ -0,0 +1,57 @@
+﻿using Dapper;
+using Hospital_ERP_Backend.Domain.Entities;
+using Hospital_ERP_Backend.Domain.Interfaces.Base;
+using Hospital_ERP_Backend.Infrastructure.Setting;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Options;
+using System.Data;
+
+namespace Hospital_ERP_Backend.Infrastructure.Repositories.Queries.Base
+{
+    public abstract class BaseQueryRepository<T> : IBaseQueryRepository<T>, IDisposable
+    {
+        protected readonly IDbConnection _connection;
+        protected readonly MySetting _setting;
+
+        protected BaseQueryRepository(IOptions<MySetting> setting)
+        {
+            _setting = setting.Value;
+            _connection = new SqlConnection(_setting.ConnectionString);
+        }
+
+        protected abstract string GetAllSpName { get; }
+        protected abstract string GetByIdSpName { get; }
+
+        public virtual async Task<IEnumerable<T>> GetAllAsync(int page)
+        {
+            var parameters = new
+            {
+                page,
+                rows = _setting.RowsPerPage
+            };
+            return await _connection.QueryAsync<T>(
+                GetAllSpName,
+                parameters,
+                commandType: CommandType.StoredProcedure
+            );
+        }
+
+        public virtual async Task<T?> GetAsync(int id)
+        {
+            var parameters = new
+            {
+                id = id
+            };
+            return await _connection.QueryFirstOrDefaultAsync<T>(
+                GetByIdSpName,
+                parameters,
+                commandType: CommandType.StoredProcedure
+            );
+        }
+
+        public void Dispose()
+        {
+            _connection?.Dispose();
+        }
+    }
+}
