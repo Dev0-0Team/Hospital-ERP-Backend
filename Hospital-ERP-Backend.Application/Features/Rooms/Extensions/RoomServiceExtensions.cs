6c011c73bc6bf99ad8401b361ace9d58f94d1a3b
ahmed <ahmedayman25606@gmail.com>
2026-07-11 17:32:04 +0200

    Implement Get Room By Id in Application Layer

--- Hospital-ERP-Backend.Application/Features/Rooms/Extensions/RoomServiceExtensions.cs
+++ Hospital-ERP-Backend.Application/Features/Rooms/Extensions/RoomServiceExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using Hospital_ERP_Backend.Application.Features.Rooms.Queries.GetAllRooms;
+using Hospital_ERP_Backend.Application.Features.Rooms.Queries.GetRoom;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Hospital_ERP_Backend.Application.Features.Rooms.Extensions
@@ -9,8 +10,10 @@ namespace Hospital_ERP_Backend.Application.Features.Rooms.Extensions
         public static IServiceCollection AddRoomServicesExtension(this IServiceCollection Services)
         {
             Services.AddScoped<IValidator<GetAllRoomsRequest>, GetAllRoomsValidator>();
+            Services.AddScoped<IValidator<GetRoomRequest>, GetRoomValidator>();
 
             Services.AddScoped<GetAllRoomsService>();
+            Services.AddScoped<GetRoomService>();
             return Services;
         }
     }
