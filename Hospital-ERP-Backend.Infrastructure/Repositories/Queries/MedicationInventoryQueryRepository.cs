36f94c555ac230d4d44f980175c42d9ca85b7509
Compiler-A <alimoussajob@gmail.com>
2026-07-11 11:27:04 +0300

    refactor(medication intentory): inheritance base query repository

--- Hospital-ERP-Backend.Infrastructure/Repositories/Queries/MedicationInventoryQueryRepository.cs
+++ Hospital-ERP-Backend.Infrastructure/Repositories/Queries/MedicationInventoryQueryRepository.cs
@@ -2,6 +2,7 @@
 using Hospital_ERP_Backend.Domain.Entities;
 using Hospital_ERP_Backend.Domain.Interfaces.Base;
 using Hospital_ERP_Backend.Infrastructure.Data;
+using Hospital_ERP_Backend.Infrastructure.Repositories.Queries.Base;
 using Hospital_ERP_Backend.Infrastructure.Setting;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
@@ -10,47 +11,12 @@ using System.Data;
 
 namespace Hospital_ERP_Backend.Infrastructure.Repositories.Queries
 {
-    public class MedicationInventoryQueryRepository : IBaseQueryRepository<MedicationInventory>, IDisposable
+    public class MedicationInventoryQueryRepository : BaseQueryRepository<MedicationInventory>
     {
-        private readonly IDbConnection _connection;
-        private readonly MySetting _setting;
+        protected override string GetAllSpName => "medication_inventories.SP_GetAllMedicationInventories";
+        protected override string GetByIdSpName => "medication_inventories.SP_GetMedicationInventoryById";
 
-        public MedicationInventoryQueryRepository( IOptions<MySetting> setting)
-        {
-            _connection = new SqlConnection(setting.Value.ConnectionString);
-            _setting = setting.Value;
-        }
+        public MedicationInventoryQueryRepository( IOptions<MySetting> setting) : base(setting) { }
 
-        public async Task<MedicationInventory?> GetAsync(int id)
-        {
-            var parameters = new
-            {
-                id
-            };
-
-            return await _connection.QueryFirstOrDefaultAsync<MedicationInventory>(
-                "medication_inventories.SP_GetMedicationInventoryById",
-                parameters,
-                commandType: CommandType.StoredProcedure);
-        }
-
-        public async Task<IEnumerable<MedicationInventory>> GetAllAsync(int page)
-        {
-            var parameters = new
-            {
-                page,
-                rows = _setting.RowsPerPage
-            };
-
-            return await _connection.QueryAsync<MedicationInventory>(
-                "medication_inventories.SP_GetAllMedicationInventories",
-                parameters,
-                commandType: CommandType.StoredProcedure);
-        }
-
-        public void Dispose()
-        {
-            _connection.Dispose();
-        }
     }
 }
