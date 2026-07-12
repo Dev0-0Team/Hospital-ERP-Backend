bc75a6c8b72ead0eab7b3df0e5e3badd992149de
Abdelrahman Owais <abdoo.owais@gmail.com>
2026-07-12 22:00:45 +0300

    feat(prescription item): Add precription item query repo and register it in DI

---
+++ Hospital-ERP-Backend.Infrastructure/Repositories/Queries/PrescriptionItemQueryRepository.cs
@@ -0,0 +1,15 @@
+﻿using Hospital_ERP_Backend.Domain.Entities;
+using Hospital_ERP_Backend.Infrastructure.Repositories.Queries.Base;
+using Hospital_ERP_Backend.Infrastructure.Setting;
+using Microsoft.Extensions.Options;
+
+namespace Hospital_ERP_Backend.Infrastructure.Repositories.Queries
+{
+    public class PrescriptionItemQueryRepository : BaseQueryRepository<PrescriptionItem>
+    {
+        protected override string GetAllSpName => "prescription_items.SP_GetAllPrescriptionItems";
+        protected override string GetByIdSpName => "prescription_items.SP_GetPrescriptionItemById";
+
+        public PrescriptionItemQueryRepository(IOptions<MySetting> setting) : base(setting) { }
+    }
+}
