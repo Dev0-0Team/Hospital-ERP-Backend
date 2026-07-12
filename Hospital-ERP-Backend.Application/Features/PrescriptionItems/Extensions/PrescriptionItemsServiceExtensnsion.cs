b2316ec0c33b3464574c9531fba16508acae54cf
Abdelrahman Owais <abdoo.owais@gmail.com>
2026-07-12 22:18:07 +0300

    feat(prescription item): Add GetPrecriptionItem query and register it in DI

---
+++ Hospital-ERP-Backend.Application/Features/PrescriptionItems/Extensions/PrescriptionItemsServiceExtensnsion.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Hospital_ERP_Backend.Application.Features.PrescriptionItems.Queries.GetPrescriptionItem;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hospital_ERP_Backend.Application.Features.PrescriptionItems.Extensions
+{
+    public static class PrescriptionItemsServiceExtension
+    {
+        public static IServiceCollection AddPrescriptionItemsServicesExtension(this IServiceCollection services)
+        {
+            services.AddScoped<IValidator<GetPrescriptionItemRequest>, GetPrescriptionItemValidator>();
+
+            services.AddScoped<GetPrescriptionItemService>();
+
+            return services;
+        }
+    }
+}
---
+++ Hospital-ERP-Backend.Application/Features/PrescriptionItems/Queries/GetPrescriptionItem/GetPrescriptionItemRequest.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Hospital_ERP_Backend.Application.Features.PrescriptionItems.Queries.GetPrescriptionItem
+{
+    public record GetPrescriptionItemRequest : IRequest<GetPrescriptionItemResponse>
+    {
+        public int Id { get; set; }
+    }
+}
