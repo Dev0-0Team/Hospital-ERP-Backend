40994a604418aa93c39256c8d0c2aad8dc913341
Abdelrahman Owais <abdoo.owais@gmail.com>
2026-07-12 22:33:12 +0300

    feat(prescription item): Add UpdatePrecriptionItem command and register it in DI

---
+++ Hospital-ERP-Backend.Application/Features/PrescriptionItems/Commands/UpdatePrescriptionItem/UpdatePrescriptionItemValidator.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+
+namespace Hospital_ERP_Backend.Application.Features.PrescriptionItems.Commands.UpdatePrescriptionItem
+{
+    public class UpdatePrescriptionItemValidator : AbstractValidator<UpdatePrescriptionItemRequest>
+    {
+        public UpdatePrescriptionItemValidator()
+        {
+            RuleFor(x => x.Id)
+                .GreaterThan(0).WithMessage("Id must be a greater than 0.");
+
+            RuleFor(x => x.PrescriptionId)
+                .GreaterThan(0).WithMessage("PrescriptionId must be a greater than 0.");
+
+            RuleFor(x => x.MedicationId)
+                .GreaterThan(0).WithMessage("MedicationId must be a greater than 0.");
+
+            RuleFor(x => x.Dosage)
+                .NotEmpty().WithMessage("Dosage is required.");
+
+            RuleFor(x => x.Duration)
+                .NotEmpty().WithMessage("Duration is required.");
+
+            RuleFor(x => x.Quantity)
+                .GreaterThan(0).WithMessage("Quantity must be a greater than 0.");
+
+            RuleFor(x => x.Instructions)
+                .MaximumLength(500).WithMessage("Instructions must be at most 500 characters long.");
+        }
+    }
+}
--- Hospital-ERP-Backend.Application/Features/PrescriptionItems/Extensions/PrescriptionItemsServiceExtensnsion.cs
+++ Hospital-ERP-Backend.Application/Features/PrescriptionItems/Extensions/PrescriptionItemsServiceExtensnsion.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using Hospital_ERP_Backend.Application.Features.PrescriptionItems.Commands.CreatePrescriptionItem;
+using Hospital_ERP_Backend.Application.Features.PrescriptionItems.Commands.UpdatePrescriptionItem;
 using Hospital_ERP_Backend.Application.Features.PrescriptionItems.Queries.GetAllPrescriptionItems;
 using Hospital_ERP_Backend.Application.Features.PrescriptionItems.Queries.GetPrescriptionItem;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,10 +14,12 @@ namespace Hospital_ERP_Backend.Application.Features.PrescriptionItems.Extensions
             services.AddScoped<IValidator<GetPrescriptionItemRequest>, GetPrescriptionItemValidator>();
             services.AddScoped<IValidator<GetAllPrescriptionItemsRequest>, GetAllPrescriptionItemsValidator>();
             services.AddScoped<IValidator<CreatePrescriptionItemRequest>, CreatePrescriptionItemValidator>();
+            services.AddScoped<IValidator<UpdatePrescriptionItemRequest>, UpdatePrescriptionItemValidator>();
 
             services.AddScoped<GetPrescriptionItemService>();
             services.AddScoped<GetAllPrescriptionItemsService>();
             services.AddScoped<CreatePrescriptionItemService>();
+            services.AddScoped<UpdatePrescriptionItemService>();
             return services;
         }
     }
