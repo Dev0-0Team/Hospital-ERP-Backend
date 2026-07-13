ce3558cb8d7303bd3eb30a9f8663a6cd071a189b
Abdelrahman Owais <abdoo.owais@gmail.com>
2026-07-13 13:48:42 +0300

    feat(lab order): Register lab order in DI

---
+++ Hospital-ERP-Backend.Application/Features/LabOrders/Extensions/LabOrdersServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using Hospital_ERP_Backend.Application.Features.LabOrders.Queries.GetLabOrder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hospital_ERP_Backend.Application.Features.LabOrders.Extensions
+{
+    public static class LabOrdersServiceExtensions
+    {
+        public static IServiceCollection AddLabOrdersServicesExtension(this IServiceCollection services)
+        {
+            services.AddScoped<IValidator<GetLabOrderRequest>, GetLabOrderValidator>();
+
+            services.AddScoped<GetLabOrderService>();
+            return services;
+        }
+    }
+}
