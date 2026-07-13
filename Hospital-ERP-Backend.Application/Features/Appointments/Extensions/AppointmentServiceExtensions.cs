95db198ea451a6dd678e7e9dc4c2284bebfa37f0
ahmed <ahmedayman25606@gmail.com>
2026-07-13 19:49:10 +0200

    Implement Create Appointment in Application Layer

--- Hospital-ERP-Backend.Application/Features/Appointments/Extensions/AppointmentServiceExtensions.cs
+++ Hospital-ERP-Backend.Application/Features/Appointments/Extensions/AppointmentServiceExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using Hospital_ERP_Backend.Application.Features.Appointments.Commands.CreateAppointment;
 using Hospital_ERP_Backend.Application.Features.Appointments.Queries.GetAllAppointments;
 using Hospital_ERP_Backend.Application.Features.Appointments.Queries.GetAppointment;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,9 +12,11 @@ namespace Hospital_ERP_Backend.Application.Features.Appointments.Extensions
         {
             services.AddScoped<IValidator<GetAppointmentRequest>, GetAppointmentValidator>();
             services.AddScoped<IValidator<GetAllAppointmentsRequest>, GetAllAppointmentsValidator>();
+            services.AddScoped<IValidator<CreateAppointmentRequest>, CreateAppointmentValidator>();
 
             services.AddScoped<GetAppointmentService>();
             services.AddScoped<GetAllAppointmentsService>();
+            services.AddScoped<CreateAppointmentService>();
             return services;
         }
     }
