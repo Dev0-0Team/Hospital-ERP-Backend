b19005655c6366cd4d035ae574974d82be44c745
Compiler-A <alimoussajob@gmail.com>
2026-07-14 15:35:01 +0300

    feature(department): add delete department service files

---
+++ Hospital-ERP-Backend.Application/Features/Departments/Commands/DeleteDepartment/DeleteDepartmentRequest.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Hospital_ERP_Backend.Application.Features.Departments.Commands.DeleteDepartment
+{
+    public record DeleteDepartmentRequest : IRequest<bool>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Hospital-ERP-Backend.Application/Features/Departments/Commands/DeleteDepartment/DeleteDepartmentValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Hospital_ERP_Backend.Application.Features.Departments.Commands.DeleteDepartment
+{
+    public class DeleteDepartmentValidator : AbstractValidator<DeleteDepartmentRequest>
+    {
+        public DeleteDepartmentValidator()
+        {
+            RuleFor(x => x.Id)
+                .GreaterThan(0)
+                .WithMessage("Department Id must be greater than 0.");
+        }
+    }
+}
