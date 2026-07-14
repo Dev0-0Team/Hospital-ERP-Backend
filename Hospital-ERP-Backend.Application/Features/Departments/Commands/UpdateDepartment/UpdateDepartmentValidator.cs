becca8407bf4c79f67c08edf3ddde77400f8675a
Compiler-A <alimoussajob@gmail.com>
2026-07-14 15:21:43 +0300

    feature(department): add update department service files

---
+++ Hospital-ERP-Backend.Application/Features/Departments/Commands/UpdateDepartment/UpdateDepartmentValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace Hospital_ERP_Backend.Application.Features.Departments.Commands.UpdateDepartment
+{
+    public class UpdateDepartmentValidator : AbstractValidator<UpdateDepartmentRequest>
+    {
+        public UpdateDepartmentValidator()
+        {
+            RuleFor(x => x.Id)
+                .GreaterThan(0)
+                .WithMessage("Department Id must be greater than 0.");
+
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name is required.")
+                .MaximumLength(100).WithMessage("Name must not exceed 100 characters.");
+
+            RuleFor(x => x.Description)
+                .NotEmpty().WithMessage("Description is required")
+                .MaximumLength(500).WithMessage("Description must not exceed 500 characters.");
+        }
+    }
+}
