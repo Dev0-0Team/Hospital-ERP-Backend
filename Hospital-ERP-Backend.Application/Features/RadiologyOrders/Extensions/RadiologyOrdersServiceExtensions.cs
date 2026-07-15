c4331fcfb0488dcd61a23aefde6487865b683d93
Abdelrahman Owais <abdoo.owais@gmail.com>
2026-07-15 13:15:20 +0300

    feat(ragiology order): Add GetAllRagioloyOrders query and register it in DI

---
+++ Hospital-ERP-Backend.Application/Features/RadiologyOrders/Extensions/RadiologyOrdersServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Hospital_ERP_Backend.Application.Features.RadiologyOrders.Queries.GetAllRadiologyOrders;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hospital_ERP_Backend.Application.Features.RadiologyOrders.Extensions
+{
+    public static class RadiologyOrdersServiceExtensions
+    {
+        public static IServiceCollection RadiologyOrdersServicesExtension(this IServiceCollection services)
+        {
+            services.AddScoped<IValidator<GetAllRadiologyOrdersRequest>, GetAllRadiologyOrdersValidator>();
+
+            services.AddScoped<GetAllRadiologyOrdersService>();
+
+
+            return services;
+        }
+    }
+}
