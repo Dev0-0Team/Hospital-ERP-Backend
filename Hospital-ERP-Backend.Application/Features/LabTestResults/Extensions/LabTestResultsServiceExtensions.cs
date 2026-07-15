5b3d8982d0025f91a6d8438165ff7f95ed1ea826
Abdelrahman Owais <abdoo.owais@gmail.com>
2026-07-15 12:23:48 +0300

    feat(lab test result): Add UpdateLabTestResult command and register it in DI

--- Hospital-ERP-Backend.Application/Features/LabTestResults/Extensions/LabTestResultsServiceExtensions.cs
+++ Hospital-ERP-Backend.Application/Features/LabTestResults/Extensions/LabTestResultsServiceExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using FluentValidation;
 using Hospital_ERP_Backend.Application.Features.LabOrders.Queries.GetAllLabOrders;
 using Hospital_ERP_Backend.Application.Features.LabTestResults.Commands.CreateLabTestResult;
+using Hospital_ERP_Backend.Application.Features.LabTestResults.Commands.UpdateLabTestResult;
 using Hospital_ERP_Backend.Application.Features.LabTestResults.Queries.GetLabTestResult;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -15,10 +16,12 @@ namespace Hospital_ERP_Backend.Application.Features.LabTestResults.Extensions
 
             services.AddScoped<IValidator<GetLabTestResultRequest>, GetLabTestResultValidator>();
             services.AddScoped<IValidator<CreateLabTestResultRequest>, CreateLabTestResultValidator>();
+            services.AddScoped<IValidator<UpdateLabTestResultRequest>, UpdateLabTestResultValidator>();
 
             services.AddScoped<GetLabTestResultService>();
             services.AddScoped<GetAllLabOrdersService>();
             services.AddScoped<CreateLabTestResultService>();
+            services.AddScoped<UpdateLabTestResultService>();
             return services;
         }
 
