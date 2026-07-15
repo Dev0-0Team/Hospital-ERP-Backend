3071aa46ca3a12aab19ed3307ff9680848f933cf
Abdelrahman Owais <abdoo.owais@gmail.com>
2026-07-15 16:42:41 +0300

    fear(radiology report): Add GetAllRadiologyReport query repo and register it in DI

---
+++ Hospital-ERP-Backend.Application/Features/RadiologyReports/Extensions/RadiologyReportsServicesExtension.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Hospital_ERP_Backend.Application.Features.RadiologyReports.Queries.GetAllRadiologyReports;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hospital_ERP_Backend.Application.Features.RadiologyReports.Extensions
+{
+    public static class RadiologyReportsServicesExtension
+    {
+        public static IServiceCollection AddRadiologyReportsServicesExtension(this IServiceCollection services)
+        {
+            services.AddScoped<IValidator<GetAllRadiologyReportsRequest>, GetAllRadiologyReportsValidator>();
+
+
+            services.AddScoped<GetAllRadiologyReportsService>();
+
+            return services;
+        }
+    }
+}
---
+++ Hospital-ERP-Backend.Application/Features/RadiologyReports/Quries/GetAllRadiologyReports/GetAllRadiologyReportsRequest.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Hospital_ERP_Backend.Application.Features.RadiologyReports.Queries.GetAllRadiologyReports
+{
+    public record GetAllRadiologyReportsRequest
+        : IRequest<IEnumerable<GetAllRadiologyReportsResponse>>
+    {
+        public int Page { get; set; }
+    }
+}
