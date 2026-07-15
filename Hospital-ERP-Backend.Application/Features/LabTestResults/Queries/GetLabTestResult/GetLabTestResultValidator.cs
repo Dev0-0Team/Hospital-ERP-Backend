beef61fbd0952bf52791996322abf58834380ed2
Abdelrahman Owais <abdoo.owais@gmail.com>
2026-07-15 12:15:40 +0300

    feat(lab test result): Add GetLabTestResult and register it in DI

---
+++ Hospital-ERP-Backend.Application/Features/LabTestResults/Queries/GetLabTestResult/GetLabTestResultValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Hospital_ERP_Backend.Application.Features.LabTestResults.Queries.GetLabTestResult
+{
+    public class GetLabTestResultValidator : AbstractValidator<GetLabTestResultRequest>
+    {
+        public GetLabTestResultValidator()
+        {
+            RuleFor(x => x.Id)
+                .GreaterThan(0)
+                .WithMessage("Lab Test Result Id must be greater than 0.");
+        }
+    }
+}
