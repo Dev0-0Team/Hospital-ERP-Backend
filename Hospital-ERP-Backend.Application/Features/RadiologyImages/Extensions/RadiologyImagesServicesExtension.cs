f193b0b92e6c6eebf437d364fbe742cf9cb0b23f
Abdelrahman Owais <abdoo.owais@gmail.com>
2026-07-15 15:05:30 +0300

    feat(radiology image): Add GetRadioloyImage query and register it in DI

--- Hospital-ERP-Backend.Application/Features/RadiologyImages/Extensions/RadiologyImagesServicesExtension.cs
+++ Hospital-ERP-Backend.Application/Features/RadiologyImages/Extensions/RadiologyImagesServicesExtension.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using Hospital_ERP_Backend.Application.Features.RadiologyImages.Queries.GetAllRadiologyImages;
+using Hospital_ERP_Backend.Application.Features.RadiologyImages.Queries.GetRadiologyImage;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Hospital_ERP_Backend.Application.Features.RadiologyImages.Extensions
@@ -10,9 +11,10 @@ namespace Hospital_ERP_Backend.Application.Features.RadiologyImages.Extensions
         public static IServiceCollection AddRadiologyImageServicesExtension(this IServiceCollection services)
         {
             services.AddScoped<IValidator<GetAllRadiologyImagesRequest>, GetAllRadiologyImagesValidator>();
-
+            services.AddScoped<IValidator<GetRadiologyImageRequest>, GetRadiologyImageValidator>();
 
             services.AddScoped<GetAllRadiologyImagesService>();
+            services.AddScoped<GetRadiologyImageService>();
             return services;
         }
     }
