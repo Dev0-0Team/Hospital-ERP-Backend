c82462143fc512049b2657dbb3f4c62254482ed1
Abdelrahman Owais <abdoo.owais@gmail.com>
2026-07-15 15:24:42 +0300

    feat(radiology image): Add DeleteRadioloyImage command and register it in DI

--- Hospital-ERP-Backend.Application/Features/RadiologyImages/Extensions/RadiologyImagesServicesExtension.cs
+++ Hospital-ERP-Backend.Application/Features/RadiologyImages/Extensions/RadiologyImagesServicesExtension.cs
@@ -1,6 +1,7 @@
 ﻿using FluentValidation;
 using Hospital_ERP_Backend.Application.Features.Appointments.Commands.CreateAppointment;
 using Hospital_ERP_Backend.Application.Features.RadiologyImages.Commands.CreateRadiologyImage;
+using Hospital_ERP_Backend.Application.Features.RadiologyImages.Commands.DeleteRadiologyImage;
 using Hospital_ERP_Backend.Application.Features.RadiologyImages.Commands.UpdateRadiologyImage;
 using Hospital_ERP_Backend.Application.Features.RadiologyImages.Queries.GetAllRadiologyImages;
 using Hospital_ERP_Backend.Application.Features.RadiologyImages.Queries.GetRadiologyImage;
@@ -17,11 +18,14 @@ namespace Hospital_ERP_Backend.Application.Features.RadiologyImages.Extensions
             services.AddScoped<IValidator<GetRadiologyImageRequest>, GetRadiologyImageValidator>();
             services.AddScoped<IValidator<CreateRadiologyImageRequest>, CreateRadiologyImageValidator>();
             services.AddScoped<IValidator<UpdateRadiologyImageRequest>, UpdateRadiologyImageValidator>();
+            services.AddScoped<IValidator<DeleteRadiologyImageRequest>, DeleteRadiologyImageValidator>();
 
             services.AddScoped<GetAllRadiologyImagesService>();
             services.AddScoped<GetRadiologyImageService>();
             services.AddScoped<CreateAppointmentService>();
             services.AddScoped<UpdateRadiologyImageService>();
+            services.AddScoped<DeleteRadiologyImageService>();
+
             return services;
         }
     }
