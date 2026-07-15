a21fc8697ae040a5112990b3213b7ebd4d7b7147
Abdelrahman Owais <abdoo.owais@gmail.com>
2026-07-15 13:28:57 +0300

    feat(ragiology order): Add CreateRadiologyOrder command and register it in DI

---
+++ Hospital-ERP-Backend.Application/Features/RadiologyOrders/Commands/CreateRadiologyOrder/CreateRadiologyOrderValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+
+namespace Hospital_ERP_Backend.Application.Features.RadiologyOrders.Commands.CreateRadiologyOrder
+{
+    public class CreateRadiologyOrderValidator
+        : AbstractValidator<CreateRadiologyOrderRequest>
+    {
+        public CreateRadiologyOrderValidator()
+        {
+            RuleFor(x => x.PatientId)
+                .GreaterThan(0).WithMessage("Patient Id must be greater than 0.");
+
+            RuleFor(x => x.DoctorId)
+                .GreaterThan(0).WithMessage("Doctor Id must be greater than 0."); ;
+
+            RuleFor(x => x.Type)
+                .NotEmpty().WithMessage("Radiology type must be not empty."); ;
+
+            RuleFor(x => x.Status)
+                .Must(x =>
+                    x == "Ordered" ||
+                    x == "Scheduled" ||
+                    x == "Completed" ||
+                    x == "Cancelled")
+                .WithMessage("Invalid status.");
+        }
+    }
+}
--- Hospital-ERP-Backend.Application/Features/RadiologyOrders/Extensions/RadiologyOrdersServiceExtensions.cs
+++ Hospital-ERP-Backend.Application/Features/RadiologyOrders/Extensions/RadiologyOrdersServiceExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using Hospital_ERP_Backend.Application.Features.Appointments.Queries.GetAllAppointments;
+using Hospital_ERP_Backend.Application.Features.RadiologyOrders.Commands.CreateRadiologyOrder;
 using Hospital_ERP_Backend.Application.Features.RadiologyOrders.Queries.GetAllRadiologyOrders;
 using Hospital_ERP_Backend.Application.Features.RadiologyOrders.Queries.GetRadiologyOrder;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,10 +13,11 @@ namespace Hospital_ERP_Backend.Application.Features.RadiologyOrders.Extensions
         {
             services.AddScoped<IValidator<GetAllRadiologyOrdersRequest>, GetAllRadiologyOrdersValidator>();
             services.AddScoped<IValidator<GetRadiologyOrderRequest>, GetRadiologyOrderValidator>();
+            services.AddScoped<IValidator<CreateRadiologyOrderRequest>, CreateRadiologyOrderValidator>();
 
             services.AddScoped<GetAllRadiologyOrdersService>();
             services.AddScoped<GetAllAppointmentsService>();
-
+            services.AddScoped<CreateRadiologyOrderService>();
 
             return services;
         }
