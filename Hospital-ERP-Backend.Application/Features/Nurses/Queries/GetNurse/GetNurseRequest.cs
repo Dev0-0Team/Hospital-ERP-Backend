125aede8a25565f49adb1e03135cd48224ddadae
Compiler-A <alimoussajob@gmail.com>
2026-07-16 16:38:11 +0300

    feature(nurse): add get nurse service in application layer

---
+++ Hospital-ERP-Backend.Application/Features/Nurses/Queries/GetNurse/GetNurseRequest.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Hospital_ERP_Backend.Application.Features.Nurses.Queries.GetNurse
+{
+    public record GetNurseRequest : IRequest<GetNurseResponse>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Hospital-ERP-Backend.Application/Features/Nurses/Queries/GetNurse/GetNurseService.cs
@@ -0,0 +1,49 @@
+﻿using FluentValidation;
+using Hospital_ERP_Backend.Application.Features.Doctors.Queries.GetDoctor;
+using Hospital_ERP_Backend.Domain.Entities;
+using Hospital_ERP_Backend.Domain.Interfaces.Base;
+using MediatR;
+
+
+namespace Hospital_ERP_Backend.Application.Features.Nurses.Queries.GetNurse
+{
+    public class GetNurseService : IRequestHandler<GetNurseRequest, GetNurseResponse>
+    {
+        private readonly IBaseQueryRepository<Nurse> _repository;
+
+        private readonly IValidator<GetNurseRequest> _validator;
+
+        public GetNurseService(
+            IBaseQueryRepository<Nurse> repository,
+            IValidator<GetNurseRequest> validator)
+        {
+            _repository = repository;
+            _validator = validator;
+        }
+
+        public async Task<GetNurseResponse> Handle(GetNurseRequest request, CancellationToken cancellationToken)
+        {
+            var validationResult = await _validator.ValidateAsync(request);
+
+            if (!validationResult.IsValid)
+            {
+                throw new ArgumentException(
+                    string.Join(", ", validationResult.Errors.Select(x => x.ErrorMessage)));
+            }
+
+            Nurse? nurse = await _repository.GetAsync(request.Id);
+
+            if (nurse == null)
+            {
+                throw new KeyNotFoundException($"Nurse with Id {request.Id} not found.");
+            }
+
+            return new GetNurseResponse
+            {
+                Id = nurse.Id,
+                PersonId = nurse.PersonId,
+                DepartmentId = nurse.DepartmentId
+            };
+        }
+    }
+}
