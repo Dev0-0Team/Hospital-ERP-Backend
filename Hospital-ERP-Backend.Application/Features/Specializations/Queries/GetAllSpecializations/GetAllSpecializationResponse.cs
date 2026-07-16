557b31a2c2bc548c60035a213340a8bf21675775
Compiler-A <alimoussajob@gmail.com>
2026-07-16 11:00:10 +0300

    feature(specialization): add get all specializations files in application layer

---
+++ Hospital-ERP-Backend.Application/Features/Specializations/Queries/GetAllSpecializations/GetAllSpecializationResponse.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace Hospital_ERP_Backend.Application.Features.Specializations.Queries.GetAllSpecializations
+{
+    public record GetAllSpecializationResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
---
+++ Hospital-ERP-Backend.Application/Features/Specializations/Queries/GetAllSpecializations/GetAllSpecializationsService.cs
@@ -0,0 +1,46 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using Hospital_ERP_Backend.Domain.Entities;
+using Hospital_ERP_Backend.Domain.Interfaces.Base;
+using MediatR;
+
+namespace Hospital_ERP_Backend.Application.Features.Specializations.Queries.GetAllSpecializations
+{
+    public class GetAllSpecializationsService : IRequestHandler<GetAllSpecializationsRequest, IEnumerable<GetAllSpecializationResponse>>
+    {
+        private readonly IBaseQueryRepository<Specialization> _repository;
+        private readonly IValidator<GetAllSpecializationsRequest> _validator;
+
+        public GetAllSpecializationsService(IBaseQueryRepository<Specialization> repository, IValidator<GetAllSpecializationsRequest> validator)
+        {
+            _repository = repository;
+            _validator = validator;
+        }
+
+        public async Task<IEnumerable<GetAllSpecializationResponse>> Handle(GetAllSpecializationsRequest request, CancellationToken cancellationToken)
+        {
+            return await GetAllSpecializationsAsync(request);
+        }
+
+        private async Task<IEnumerable<GetAllSpecializationResponse>> GetAllSpecializationsAsync(GetAllSpecializationsRequest request)
+        {
+            ValidationResult validationResult = await _validator.ValidateAsync(request);
+            if (!validationResult.IsValid)
+            {
+                throw new ArgumentException($"Invalid request: {string.Join(", ", validationResult.Errors.Select(e => e.ErrorMessage))}");
+            }
+
+            IEnumerable<Specialization> list = await _repository.GetAllAsync(request.Page);
+            if (list == null || list.Count() == 0)
+            {
+                throw new KeyNotFoundException($"No specializations found on page {request.Page}.");
+            }
+
+            return list.Select(r => new GetAllSpecializationResponse
+            {
+                Id = r.Id,
+                Name = r.Name
+            });
+        }
+    }
+}
