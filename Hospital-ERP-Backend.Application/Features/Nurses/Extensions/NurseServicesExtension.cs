e167dceee5ecffa32f795c546cb54022e028b580
Compiler-A <alimoussajob@gmail.com>
2026-07-16 16:49:06 +0300

    feature(nurse): add dependency injection nurse services in application layer

---
+++ Hospital-ERP-Backend.Application/Features/Nurses/Extensions/NurseServicesExtension.cs
@@ -0,0 +1,31 @@
+﻿
+
+using FluentValidation;
+using Hospital_ERP_Backend.Application.Features.Nurses.Commands.CreateNurse;
+using Hospital_ERP_Backend.Application.Features.Nurses.Commands.DeleteNurse;
+using Hospital_ERP_Backend.Application.Features.Nurses.Commands.UpdateNurse;
+using Hospital_ERP_Backend.Application.Features.Nurses.Queries.GetAllNurses;
+using Hospital_ERP_Backend.Application.Features.Nurses.Queries.GetNurse;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hospital_ERP_Backend.Application.Features.Nurses.Extensions
+{
+    public static class NurseServicesExtension
+    {
+        public static IServiceCollection AddNurseServiceExtensions(this IServiceCollection services)
+        {
+            services.AddScoped<IValidator<GetNurseRequest>, GetNurseValidator>();
+            services.AddScoped<IValidator<GetAllNursesRequest>, GetAllNursesValidator>();
+            services.AddScoped<IValidator<CreateNurseRequest>, CreateNurseValidator>();
+            services.AddScoped<IValidator<UpdateNurseRequest>, UpdateNurseValidator>();
+            services.AddScoped<IValidator<DeleteNurseRequest>, DeleteNurseValidator>();
+
+            services.AddScoped<GetAllNursesService>();
+            services.AddScoped<GetNurseService>();
+            services.AddScoped<UpdateNurseService>();
+            services.AddScoped<DeleteNurseService>();
+            services.AddScoped<CreateNurseService>();
+            return services;
+        }
+    }
+}
