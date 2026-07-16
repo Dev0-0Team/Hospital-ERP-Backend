61dbf61fa0a387b47b8d0c43e6a629df36aa4b0c
Abdelrahman Owais <abdoo.owais@gmail.com>
2026-07-16 13:07:56 +0300

    feat(doctor): Add doctor query repo and register it in DI

---
+++ Hospital-ERP-Backend.Infrastructure/Repositories/Queries/DoctorQueryRepository.cs
@@ -0,0 +1,16 @@
+﻿using Hospital_ERP_Backend.Domain.Entities;
+using Hospital_ERP_Backend.Infrastructure.Repositories.Queries.Base;
+using Hospital_ERP_Backend.Infrastructure.Setting;
+using Microsoft.Extensions.Options;
+
+namespace Hospital_ERP_Backend.Infrastructure.Repositories.Queries
+{
+    public class DoctorQueryRepository : BaseQueryRepository<Doctor>
+    {
+        protected override string GetAllSpName => "doctors.SP_GetAllDoctors";
+        protected override string GetByIdSpName => "doctors.SP_GetDoctorById";
+
+        public DoctorQueryRepository(IOptions<MySetting> setting) : base(setting) { }
+
+    }
+}
