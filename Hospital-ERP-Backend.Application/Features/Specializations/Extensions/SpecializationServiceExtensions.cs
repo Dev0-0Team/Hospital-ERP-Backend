660886ff47a27ccdc0404856fa4152b9cf83eee1
Compiler-A <alimoussajob@gmail.com>
2026-07-16 11:21:51 +0300

    feature(specialization): add Dependency injection specialization in specialization service extensions

---
+++ Hospital-ERP-Backend.Application/Features/Specializations/Extensions/SpecializationServiceExtensions.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using Hospital_ERP_Backend.Application.Features.Specializations.Commands.CreateSpecialization;
+using Hospital_ERP_Backend.Application.Features.Specializations.Commands.DeleteSpecialization;
+using Hospital_ERP_Backend.Application.Features.Specializations.Commands.UpdateSpecialization;
+using Hospital_ERP_Backend.Application.Features.Specializations.Queries.GetAllSpecializations;
+using Hospital_ERP_Backend.Application.Features.Specializations.Queries.GetSpecialization;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hospital_ERP_Backend.Application.Features.Specializations.Extensions
+{
+    public static class SpecializationServiceExtensions
+    {
+        public static IServiceCollection AddRoomTypeServicesExtension(this IServiceCollection Services)
+        {
+            Services.AddScoped<IValidator<GetAllSpecializationsRequest>, GetAllSpecializationsValidator>();
+            Services.AddScoped<IValidator<GetSpecializationRequest>, GetSpecializationValidator>();
+            Services.AddScoped<IValidator<CreateSpecializationRequest>, CreateSpecializationValidator>();
+            Services.AddScoped<IValidator<UpdateSpecializationRequest>, UpdateSpecializationValidator>();
+            Services.AddScoped<IValidator<DeleteSpecializationRequest>, DeleteSpecializationValidator>();
+
+            Services.AddScoped<GetAllSpecializationsService>();
+            Services.AddScoped<GetSpecializationService>();
+            Services.AddScoped<CreateSpecializationService>();
+            Services.AddScoped<UpdateSpecializationService>();
+            Services.AddScoped<DeleteSpecializationService>();
+            return Services;
+        }
+    }
+}
