72ada926ac9cbdf139f147dda5bdbb875f136aa9
ahmed <ahmedayman25606@gmail.com>
2026-07-16 20:35:07 +0200

    Implement Room Assignment Command & Query Repository

--- Hospital-ERP-Backend.Infrastructure/Repositories/Queries/Extensions/QueryRepositoriesExtensions.cs
+++ Hospital-ERP-Backend.Infrastructure/Repositories/Queries/Extensions/QueryRepositoriesExtensions.cs
@@ -35,6 +35,7 @@ namespace Hospital_ERP_Backend.Infrastructure.Repositories.Queries.Extensions
             services.AddScoped<IBaseQueryRepository<Doctor>, DoctorQueryRepository>();
             services.AddScoped<IBaseQueryRepository<Specialization>, SpecializationQueryRepository>();
             services.AddScoped<IBaseQueryRepository<Nurse>, NurseQueryRepository>();
+            services.AddScoped<IBaseQueryRepository<RoomAssignment>, RoomAssignmentQueryRepository>();
             return services;
         }
     }
