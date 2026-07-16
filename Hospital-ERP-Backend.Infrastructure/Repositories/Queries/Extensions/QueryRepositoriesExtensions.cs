c1fd8f9b23333dfd2016b6402dae2f697121cbdd
Compiler-A <alimoussajob@gmail.com>
2026-07-16 15:14:48 +0300

    feature(nurse): add nurse query in repository

--- Hospital-ERP-Backend.Infrastructure/Repositories/Queries/Extensions/QueryRepositoriesExtensions.cs
+++ Hospital-ERP-Backend.Infrastructure/Repositories/Queries/Extensions/QueryRepositoriesExtensions.cs
@@ -34,6 +34,7 @@ namespace Hospital_ERP_Backend.Infrastructure.Repositories.Queries.Extensions
             services.AddScoped<IBaseQueryRepository<RadiologyOrder>, RadiologyOrderQueryRepository>();
             services.AddScoped<IBaseQueryRepository<Doctor>, DoctorQueryRepository>();
             services.AddScoped<IBaseQueryRepository<Specialization>, SpecializationQueryRepository>();
+            services.AddScoped<IBaseQueryRepository<Nurse>, NurseQueryRepository>();
             return services;
         }
     }
