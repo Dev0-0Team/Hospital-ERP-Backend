d9bce4375d5b6a562953867b0fc97e76dbe01e6e
Compiler-A <alimoussajob@gmail.com>
2026-07-16 09:29:16 +0300

    feature(specialization): add delete specialization files in application layer

---
+++ Hospital-ERP-Backend.Application/Features/Specializations/Command/DeleteSpecialization/DeleteSpecializationRequest.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+
+namespace Hospital_ERP_Backend.Application.Features.Specializations.Command.DeleteSpecialization
+{
+    public record DeleteSpecializationRequest : IRequest<bool>
+    {
+        public int Id { get; set; }
+    }
+}
