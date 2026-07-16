9bba52315a60c83fac3cfc21442e258028ea549d
Compiler-A <alimoussajob@gmail.com>
2026-07-16 11:17:28 +0300

    feature(specialization): add get specialization files in application layer

---
+++ Hospital-ERP-Backend.Application/Features/Specializations/Queries/GetSpecialization/GetSpecializationResponse.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace Hospital_ERP_Backend.Application.Features.Specializations.Queries.GetSpecialization
+{
+    public record GetSpecializationResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
