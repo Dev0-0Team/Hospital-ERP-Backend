45c7db52279448ee6e690dc50a547b57e7591c31
ahmed <ahmedayman25606@gmail.com>
2026-07-16 19:22:40 +0200

    Implement Create Appointment Queue in Application Layer

---
+++ Hospital-ERP-Backend.Application/Features/AppointmentQueues/Commands/CreateAppointmentQueue/CreateAppointmentQueueService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital_ERP_Backend.Application.Features.AppointmentQueues.Commands.CreateAppointmentQueue
+{
+    internal class CreateAppointmentQueueService
+    {
+    }
+}
