ca490e9708f321b33d38a46d6336e7309bcfe408
Compiler-A <alimoussajob@gmail.com>
2026-07-16 16:25:16 +0300

    feature(nurse): add get all nurse service in application layer

---
+++ Hospital-ERP-Backend.Application/Features/Nurses/Queries/GetAllNurses/GetAllNursesResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital_ERP_Backend.Application.Features.Nurses.Queries.GetAllNurses
+{
+    public record GetAllNursesResponse
+    {
+        public int Id { get; set; }
+        public int PersonId { get; set; }
+        public int DepartmentId { get; set; }
+    }
+}
