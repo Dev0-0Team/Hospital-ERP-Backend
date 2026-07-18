a2d159e49dad30d0a5463bdd188dc940fe7875c9
Ammar <ammar45644@gmail.com>
2026-07-18 16:29:44 +0300

    feat(patients): implement patient repositories and register query dependencies

---
+++ Hospital-ERP-Backend.Infrastructure/Repositories/Commands/PatientsCommandRepository.cs
@@ -0,0 +1,14 @@
+﻿using Hospital_ERP_Backend.Domain.Entities;
+using Hospital_ERP_Backend.Infrastructure.Data;
+using Hospital_ERP_Backend.Infrastructure.Repositories.Commands.Base;
+using System;
+
+
+namespace Hospital_ERP_Backend.Infrastructure.Repositories.Commands
+{
+    public class PatientsCommandRepository : BaseCommandRepository<Patient>
+    {
+        public PatientsCommandRepository(HospitalDbContext dbContext) : base(dbContext) { }
+
+    }
+}
---
+++ Hospital-ERP-Backend.Infrastructure/Repositories/Queries/PatientQueryRepository.cs
@@ -0,0 +1,18 @@
+﻿using Dapper;
+using Hospital_ERP_Backend.Domain.Entities;
+using Hospital_ERP_Backend.Infrastructure.Repositories.Queries.Base;
+using Hospital_ERP_Backend.Infrastructure.Setting;
+using Microsoft.Extensions.Options;
+using System.Data;
+
+namespace Hospital_ERP_Backend.Infrastructure.Repositories.Queries
+{
+    public class PatientQueryRepository : BaseQueryRepository<Patient>
+    {
+        protected override string GetAllSpName => "dbo.SP_Patients_GetAll";
+        protected override string GetByIdSpName => "dbo.SP_Patients_GetById";
+
+        public PatientQueryRepository(IOptions<MySetting> setting) : base(setting) { }
+
+    }
+}
