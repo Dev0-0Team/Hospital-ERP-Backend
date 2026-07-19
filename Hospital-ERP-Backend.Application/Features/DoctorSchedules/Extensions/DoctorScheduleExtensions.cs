db56783092465ecfac8726a5f95d3acd524ad977
Abdelrahman Owais <abdoo.owais@gmail.com>
2026-07-19 13:36:35 +0300

    feat(doctor schedule): Add GetAll query and register it in DI

---
+++ Hospital-ERP-Backend.Application/Features/DoctorSchedules/Extensions/DoctorScheduleExtensions.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using Hospital_ERP_Backend.Application.Features.DoctorSchedules.Queries.GetAllDoctorSchedules;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hospital_ERP_Backend.Application.Features.DoctorSchedules.Extensions
+{
+    public static class DoctorScheduleExtensions
+    {
+
+        public static IServiceCollection AddDoctorScheduleServicesExtension(this IServiceCollection services)
+        {
+            services.AddScoped<IValidator<GetAllDoctorSchedulesRequest>, GetAllDoctorSchedulesValidator>();
+
+
+            services.AddScoped<GetAllDoctorSchedulesService>();
+
+            return services;
+        }
+    }
+}
