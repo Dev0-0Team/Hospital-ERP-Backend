26fed4371d8e2dd284fea35cc7e921c3abf83e06
Compiler-A <alimoussajob@gmail.com>
2026-07-19 13:18:16 +0300

    feature(payment method): add get payment method files service

---
+++ Hospital-ERP-Backend.Application/Features/PaymentMethods/Queries/GetPaymentMethod/GetPaymentMethodRequest.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+
+namespace Hospital_ERP_Backend.Application.Features.PaymentMethods.Queries.GetPaymentMethod
+{
+    public record GetPaymentMethodRequest : IRequest<GetPaymentMethodResponse>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Hospital-ERP-Backend.Application/Features/PaymentMethods/Queries/GetPaymentMethod/GetPaymentMethodService.cs
@@ -0,0 +1,50 @@
+﻿
+
+using FluentValidation;
+using Hospital_ERP_Backend.Application.Features.Permissions.Queries.GetPermission;
+using Hospital_ERP_Backend.Domain.Entities;
+using Hospital_ERP_Backend.Domain.Interfaces.Base;
+using MediatR;
+using System.Security;
+
+namespace Hospital_ERP_Backend.Application.Features.PaymentMethods.Queries.GetPaymentMethod
+{
+    public class GetPaymentMethodService : IRequestHandler<GetPaymentMethodRequest, GetPaymentMethodResponse>
+    {
+        private readonly IBaseQueryRepository<PaymentMethod> _repository;
+        private readonly IValidator<GetPaymentMethodRequest> _validator;
+
+        public GetPaymentMethodService(IBaseQueryRepository<PaymentMethod> repository, IValidator<GetPaymentMethodRequest> validator)
+        {
+            _repository = repository;
+            _validator = validator;
+        }
+
+        public async Task<GetPaymentMethodResponse> Handle(GetPaymentMethodRequest request, CancellationToken cancellationToken)
+        {
+            return await GetPaymentMethodAsync(request);
+        }
+
+        private async Task<GetPaymentMethodResponse> GetPaymentMethodAsync(GetPaymentMethodRequest request)
+        {
+            var validationResult = await _validator.ValidateAsync(request);
+            if (!validationResult.IsValid)
+            {
+                throw new ArgumentException($"Invalid request: {string.Join(", ", validationResult.Errors.Select(e => e.ErrorMessage))}");
+            }
+
+            PaymentMethod? paymentMethod = await _repository.GetAsync(request.Id);
+            if (paymentMethod == null)
+            {
+                throw new KeyNotFoundException($"Payment method with Id {request.Id} not found.");
+            }
+
+            return new GetPaymentMethodResponse
+            {
+                Id = paymentMethod.Id,
+                Name = paymentMethod.Name
+            };
+        }
+
+    }
+}
