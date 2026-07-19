85a6d692d360f828afe729f6f2cfa6d1aaf2c14c
Abdelrahman Owais <abdoo.owais@gmail.com>
2026-07-19 13:18:48 +0300

    feat(doctor schedule): Add enum for day of week

---
+++ Hospital-ERP-Backend.Domain/Enums/DoctorScheduleDayOfWeek.cs
@@ -0,0 +1,13 @@
+﻿namespace Hospital_ERP_Backend.Domain.Enums
+{
+    public enum DoctorScheduleDayOfWeek
+    {
+        Saturday,
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday
+    }
+}
