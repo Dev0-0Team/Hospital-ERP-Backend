38f806167d8ad25a8c83c494294c0d9ee6c258a1
Compiler-A <alimoussajob@gmail.com>
2026-07-19 12:16:17 +0300

    feature(payment method): add file payment method query repository:

---
+++ Hospital-ERP-Backend.Infrastructure/Repositories/Queries/PaymentMethodQueryRepository.cs
@@ -0,0 +1,16 @@
+﻿
+using Hospital_ERP_Backend.Domain.Entities;
+using Hospital_ERP_Backend.Infrastructure.Repositories.Queries.Base;
+using Hospital_ERP_Backend.Infrastructure.Setting;
+using Microsoft.Extensions.Options;
+
+namespace Hospital_ERP_Backend.Infrastructure.Repositories.Queries
+{
+    public class PaymentMethodQueryRepository : BaseQueryRepository<PaymentMethod>
+    {
+        protected override string GetAllSpName => "payment_methods.SP_GetAllPaymentMethods";
+        protected override string GetByIdSpName => "payment_methods.SP_GetPaymentMethodById";
+
+        public PaymentMethodQueryRepository(IOptions<MySetting> setting) : base(setting) { }
+    }
+}
