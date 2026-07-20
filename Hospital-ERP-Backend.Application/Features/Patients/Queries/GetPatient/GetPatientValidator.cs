fc172e1d6b9e6150316493b53df2c22ab8dc0d30
Compiler-A <alimoussajob@gmail.com>
2026-07-20 19:31:47 +0300

    feature(patient): add get patient service files in application layer

---
+++ Hospital-ERP-Backend.Application/Features/Patients/Queries/GetPatient/GetPatientValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+
+namespace Hospital_ERP_Backend.Application.Features.Patients.Queries.GetPatient
+{
+    public class GetPatientValidator : AbstractValidator<GetPatientRequest>
+    {
+        public GetPatientValidator()
+        {
+            RuleFor(x => x.Id)
+               .GreaterThan(0).WithMessage("ID must be greater than 0.");
+        }
+    }
+}
