b854c76cf622756f0150cf5cd73f870bde89daa8
Compiler-A <alimoussajob@gmail.com>
2026-07-20 18:43:41 +0300

    feature(patient): add create patient service files in application layer

---
+++ Hospital-ERP-Backend.Application/Features/Patients/Commands/CreatePatient/CreatePatientRequest.cs
@@ -0,0 +1,13 @@
+﻿
+
+using MediatR;
+using Microsoft.Identity.Client;
+
+namespace Hospital_ERP_Backend.Application.Features.Patients.Commands.CreatePatient
+{
+    public record CreatePatientRequest : IRequest<CreatePatientResponse>
+    {
+        public int PersonId { get; set; }
+        public string? BloodType { get; set; }
+    }
+}
