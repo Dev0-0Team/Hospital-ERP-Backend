c94e5405a43acbfb60d7197128cacc55cdb4d9c1
Compiler-A <alimoussajob@gmail.com>
2026-07-20 19:02:43 +0300

    feature(patient): add delete patient service files in application layer

---
+++ Hospital-ERP-Backend.Application/Features/Patients/Commands/DeletePatient/DeletePatientRequest.cs
@@ -0,0 +1,11 @@
+﻿
+
+using MediatR;
+
+namespace Hospital_ERP_Backend.Application.Features.Patients.Commands.DeletePatient
+{
+    public record DeletePatientRequest : IRequest<bool>
+    {
+        public int Id { get; set; }
+    }
+}
