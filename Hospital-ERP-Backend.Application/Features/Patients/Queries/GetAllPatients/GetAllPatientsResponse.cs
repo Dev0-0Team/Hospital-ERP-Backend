e9977401784a335b5126e947ec676ceb464ede59
Compiler-A <alimoussajob@gmail.com>
2026-07-20 19:17:00 +0300

    feature(patient): add get all patients service files in application layer

---
+++ Hospital-ERP-Backend.Application/Features/Patients/Queries/GetAllPatients/GetAllPatientsResponse.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace Hospital_ERP_Backend.Application.Features.Patients.Queries.GetAllPatients
+{
+    public record GetAllPatientsResponse
+    {
+        public int Id { get; set; }
+        public int PersonId { get; set; }
+        public string? BloodType { get; set; }
+    }
+}
