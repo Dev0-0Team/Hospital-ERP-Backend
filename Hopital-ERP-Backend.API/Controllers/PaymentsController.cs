bea71a2d3afb451b6800171fd773a8e185ff99a4
Compiler-A <alimoussajob@gmail.com>
2026-07-23 17:55:17 +0300

    feature(payment): add payment controller in api layer

---
+++ Hopital-ERP-Backend.API/Controllers/PaymentsController.cs
@@ -0,0 +1,74 @@
+﻿using Hospital_ERP_Backend.API;
+using Hospital_ERP_Backend.API.Controllers;
+using Hospital_ERP_Backend.Application.Features.Payments.Commands.CreatePayment;
+using Hospital_ERP_Backend.Application.Features.Payments.Commands.DeletePayment;
+using Hospital_ERP_Backend.Application.Features.Payments.Commands.UpdatePayment;
+using Hospital_ERP_Backend.Application.Features.Payments.Queries.GetAllPayments;
+using Hospital_ERP_Backend.Application.Features.Payments.Queries.GetPayment;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hopital_ERP_Backend.API.Controllers
+{
+    [Route("api/Payments")]
+    [ApiController]
+    public class PaymentsController : BaseController
+    {
+        private readonly ISender _sender;
+
+        public PaymentsController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpGet(Name = "GetAllPaymentsAsync")]
+        public async Task<ActionResult<ApiResponse<IEnumerable<GetAllPaymentsResponse>?>>> GetAllAsync([FromQuery] int page = 1)
+        {
+            GetAllPaymentsRequest newRequest = new GetAllPaymentsRequest()
+            {
+                Page = page
+            };
+
+            var list = await _sender.Send(newRequest);
+            return CreateResponse<IEnumerable<GetAllPaymentsResponse>?>(list, StatusCodes.Status200OK, $"Row: {list.Count()}");
+        }
+
+
+        [HttpGet("{ID}", Name = "GetPaymentByID")]
+        public async Task<ActionResult<ApiResponse<GetPaymentResponse?>>> GetByIDAsync([FromRoute] int ID)
+        {
+            GetPaymentRequest newRequest = new GetPaymentRequest
+            {
+                Id = ID
+            };
+            var response = await _sender.Send(newRequest);
+            return CreateResponse<GetPaymentResponse?>(response, StatusCodes.Status200OK, "Found Successfully!");
+        }
+
+        [HttpPost(Name = "CreatePaymentAsync")]
+        public async Task<ActionResult<ApiResponse<CreatePaymentResponse>>> CreateAsync([FromBody] CreatePaymentRequest request)
+        {
+
+            var success = await _sender.Send(request);
+            return CreatedAtRoute("GetPaymentByID", new { ID = success!.Id }, success);
+        }
+
+        [HttpPut(Name = "UpdatePaymentAsync")]
+        public async Task<ActionResult<ApiResponse<UpdatePaymentResponse>>> UpdateAsync([FromBody] UpdatePaymentRequest request)
+        {
+            var response = await _sender.Send(request);
+            return CreateResponse<UpdatePaymentResponse>(response, StatusCodes.Status200OK, "Payment Updated Successfully!");
+        }
+
+        [HttpDelete("{ID}", Name = "DeletePaymentAsync")]
+        public async Task<ActionResult<ApiResponse<bool>>> DeleteAsync([FromRoute] int ID)
+        {
+            DeletePaymentRequest newRequest = new DeletePaymentRequest
+            {
+                Id = ID
+            };
+            var success = await _sender.Send(newRequest);
+            return CreateResponse<bool>(success, StatusCodes.Status200OK, "Payment Deleted Successfully!");
+        }
+    }
+}
