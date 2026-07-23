c42cdfa0395ae9f82dd213cce7b67732022940ce
Compiler-A <alimoussajob@gmail.com>
2026-07-23 17:42:17 +0300

    feature(payment): add get payment service in application layer

---
+++ Hospital-ERP-Backend.Application/Features/Payments/Queries/GetPayment/GetPaymentRequest.cs
@@ -0,0 +1,11 @@
+﻿
+
+using MediatR;
+
+namespace Hospital_ERP_Backend.Application.Features.Payments.Queries.GetPayment
+{
+    public record GetPaymentRequest : IRequest<GetPaymentResponse>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Hospital-ERP-Backend.Application/Features/Payments/Queries/GetPayment/GetPaymentValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Hospital_ERP_Backend.Application.Features.Payments.Queries.GetPayment
+{
+    public class GetPaymentValidator : AbstractValidator<GetPaymentRequest>
+    {
+        public GetPaymentValidator()
+        {
+            RuleFor(x => x.Id)
+                .GreaterThan(0).WithMessage("ID must be greater than 0.");
+        }
+    }
+}
