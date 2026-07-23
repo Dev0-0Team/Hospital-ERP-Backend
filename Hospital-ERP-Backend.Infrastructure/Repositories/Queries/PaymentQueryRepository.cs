0e2a5d5419efa6b634f3f2a1dc079eef872af4ea
Compiler-A <alimoussajob@gmail.com>
2026-07-23 16:49:36 +0300

    feature(payment): add query repository file

---
+++ Hospital-ERP-Backend.Infrastructure/Repositories/Queries/PaymentQueryRepository.cs
@@ -0,0 +1,16 @@
+﻿using Hospital_ERP_Backend.Domain.Entities;
+using Hospital_ERP_Backend.Infrastructure.Repositories.Queries.Base;
+using Hospital_ERP_Backend.Infrastructure.Setting;
+using Microsoft.Extensions.Options;
+
+
+namespace Hospital_ERP_Backend.Infrastructure.Repositories.Queries
+{
+    public class PaymentQueryRepository : BaseQueryRepository<Payment>
+    {
+        protected override string GetAllSpName => "payments.SP_GetAllPayments";
+        protected override string GetByIdSpName => "payments.SP_GetPaymentById";
+
+        public PaymentQueryRepository(IOptions<MySetting> setting) : base(setting) { }
+    }
+}
