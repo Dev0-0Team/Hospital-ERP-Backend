4d51d89527d7b3d8b9db0ea45d3c3376d6877c85
Compiler-A <alimoussajob@gmail.com>
2026-07-23 17:05:22 +0300

    feature(payment): add create payment service in application layer

---
+++ Hospital-ERP-Backend.Application/Features/Payments/Commands/CreatePayment/CreatePaymentResponse.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace Hospital_ERP_Backend.Application.Features.Payments.Commands.CreatePayment
+{
+    public record CreatePaymentResponse
+    {
+        public int Id { get; set; }
+        public int InvoiceId { get; set; }
+
+        public int PaymentMethodId { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public DateTime PaidAt { get; set; }
+    }
+}
