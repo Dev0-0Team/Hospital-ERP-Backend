4f0e4d0876e92ef187ea7cc375961a9385463697
Compiler-A <alimoussajob@gmail.com>
2026-07-23 17:33:57 +0300

    feature(payment): add get all payments service in application layer

---
+++ Hospital-ERP-Backend.Application/Features/Payments/Queries/GetAllPayments/GetAllPaymentsResponse.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Hospital_ERP_Backend.Application.Features.Payments.Queries.GetAllPayments
+{
+    public record GetAllPaymentsResponse
+    {
+        public int Id { get; set; }
+        public int InvoiceId { get; set; }
+        public int PaymentMethodId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime PaidAt { get; set; }
+    }
+}
