89af5ed7c505427fcfda2b4dfc2647ee0c2c9245
Abdelrahman Owais <abdoo.owais@gmail.com>
2026-07-25 02:14:34 +0300

    feat(Invoice-Items): Add GetAll query and register it in DI

--- Hospital-ERP-Backend.Application/Features/InvoiceItems/Extensions/InvoiceItemsServiceExtensions.cs
+++ Hospital-ERP-Backend.Application/Features/InvoiceItems/Extensions/InvoiceItemsServiceExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using Hospital_ERP_Backend.Application.Features.InvoiceItems.Queries.GetAllInvoiceItems;
 using Hospital_ERP_Backend.Application.Features.InvoiceItems.Queries.GetInvoiceItem;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -10,9 +11,11 @@ namespace Hospital_ERP_Backend.Application.Features.InvoiceItems.Extensions
         public static IServiceCollection AddInvoiceItemsServicesExtension(this IServiceCollection services)
         {
             services.AddScoped<IValidator<GetInvoiceItemRequest>, GetInvoiceItemValidator>();
+            services.AddScoped<IValidator<GetAllInvoiceItemsRequest>, GetAllInvoiceItemsValidator>();
 
 
             services.AddScoped<GetInvoiceItemService>();
+            services.AddScoped<GetAllInvoiceItemsService>();
             return services;
         }
     }
