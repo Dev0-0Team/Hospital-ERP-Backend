8e11f0d2998bb1ec36e93aa14ccbf5c1e498e96b
Abdelrahman Owais <abdoo.owais@gmail.com>
2026-07-25 02:09:51 +0300

    feat(Invoice-Items): Add GetById query and register it in DI

---
+++ Hospital-ERP-Backend.Application/Features/InvoiceItems/Extensions/InvoiceItemsServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Hospital_ERP_Backend.Application.Features.InvoiceItems.Queries.GetInvoiceItem;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hospital_ERP_Backend.Application.Features.InvoiceItems.Extensions
+{
+    public static class InvoiceItemsServiceExtensions
+    {
+
+        public static IServiceCollection AddInvoiceItemsServicesExtension(this IServiceCollection services)
+        {
+            services.AddScoped<IValidator<GetInvoiceItemRequest>, GetInvoiceItemValidator>();
+
+
+            services.AddScoped<GetInvoiceItemService>();
+            return services;
+        }
+    }
+}
