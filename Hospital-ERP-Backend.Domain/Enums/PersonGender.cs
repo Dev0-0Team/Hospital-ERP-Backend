b358f405463ec62d5a4997ceb5d305f97064c25f
Compiler-A <alimoussajob@gmail.com>
2026-07-26 12:16:44 +0300

    feature(person): add enum gender for person

---
+++ Hospital-ERP-Backend.Domain/Enums/PersonGender.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital_ERP_Backend.Domain.Enums
+{
+    public enum PersonGender
+    {
+        Male,
+        Female,
+        Other
+    }
+}
