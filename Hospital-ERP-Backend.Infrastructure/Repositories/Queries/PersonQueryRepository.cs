2850fb98c6e1ec5812f8d86168aa2d85480a0ee9
Compiler-A <alimoussajob@gmail.com>
2026-07-26 11:43:25 +0300

    fix(person): changing the access modifier classes repositories

--- Hospital-ERP-Backend.Infrastructure/Repositories/Queries/PersonQueryRepository.cs
+++ Hospital-ERP-Backend.Infrastructure/Repositories/Queries/PersonQueryRepository.cs
@@ -1,24 +1,17 @@
-﻿using Dapper;
-using Hospital_ERP_Backend.Domain.Entities;
-using Hospital_ERP_Backend.Domain.Interfaces.Base;
-using Hospital_ERP_Backend.Infrastructure.Data;
-using Hospital_ERP_Backend.Infrastructure.Repositories.Commands.Base;
+﻿using Hospital_ERP_Backend.Domain.Entities;
 using Hospital_ERP_Backend.Infrastructure.Repositories.Queries.Base;
 using Hospital_ERP_Backend.Infrastructure.Setting;
-using Microsoft.Data.SqlClient;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
-using System.Data;
 
 
 namespace Hospital_ERP_Backend.Infrastructure.Repositories.Queries
 {
-    public class PersonQueryRepository : BaseQueryRepository<Person>
+    internal class PersonQueryRepository : BaseQueryRepository<Person>
     {
         protected override string GetAllSpName => "persons.SP_GetAllPersons";
         protected override string GetByIdSpName => "persons.SP_GetPersonById";
 
-        public PersonQueryRepository(IOptions<MySetting> setting): base(setting)
+        internal PersonQueryRepository(IOptions<MySetting> setting): base(setting)
         { }
     }
 }
