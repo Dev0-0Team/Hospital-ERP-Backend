0c38d147388e2e1def9d2334b0b12f1199ad354b
Abdelrahman Owais <abdoo.owais@gmail.com>
2026-07-30 14:22:06 +0300

    feat(allergies): Add GetAll query and register it in DI

--- Hospital-ERP-Backend.Application/Features/Allergies/Extensions/AllergyServiceExtensions.cs
+++ Hospital-ERP-Backend.Application/Features/Allergies/Extensions/AllergyServiceExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using Hospital_ERP_Backend.Application.Features.Allergies.Queries.GetAllAllergies;
 using Hospital_ERP_Backend.Application.Features.Allergies.Queries.GetAllergy;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -9,8 +10,10 @@ namespace Hospital_ERP_Backend.Application.Features.Allergies.Extensions
         public static IServiceCollection AddAllergyServicesExtension(this IServiceCollection service)
         {
             service.AddScoped<IValidator<GetAllergyRequest>, GetAllergyValidator>();
+            service.AddScoped<IValidator<GetAllAllergiesRequest>, GetAllAllergiesValidator>();
 
             service.AddScoped<GetAllergyService>();
+            service.AddScoped<GetAllAllergiesService>();
 
             return service;
         }
