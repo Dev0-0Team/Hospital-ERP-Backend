4c05039ca558911531393c27b6e8455882f85984
Abdelrahman Owais <abdoo.owais@gmail.com>
2026-07-30 14:19:34 +0300

    feat(allergies): Add GetById querY and register it in DI

---
+++ Hospital-ERP-Backend.Application/Features/Allergies/Extensions/AllergyServiceExtensions.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Hospital_ERP_Backend.Application.Features.Allergies.Queries.GetAllergy;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hospital_ERP_Backend.Application.Features.Allergies.Extensions
+{
+    public static class AllergyServiceExtensions
+    {
+        public static IServiceCollection AddAllergyServicesExtension(this IServiceCollection service)
+        {
+            service.AddScoped<IValidator<GetAllergyRequest>, GetAllergyValidator>();
+
+            service.AddScoped<GetAllergyService>();
+
+            return service;
+        }
+    }
+}
---
+++ Hospital-ERP-Backend.Application/Features/Allergies/Queries/GetAllergy/GetAllergyResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Hospital_ERP_Backend.Application.Features.Allergies.Queries.GetAllergy
+{
+    public record GetAllergyResponse
+    {
+        public int Id { get; set; }
+
+        public int PatientId { get; set; }
+
+        public string AllergyName { get; set; } = null!;
+
+        public string Severity { get; set; } = null!;
+    }
+}
