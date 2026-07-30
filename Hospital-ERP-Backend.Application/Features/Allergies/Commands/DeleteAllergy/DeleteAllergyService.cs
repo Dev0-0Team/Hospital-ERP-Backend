d9db7162d8c9d74849262da32a9de035cbd60f24
Abdelrahman Owais <abdoo.owais@gmail.com>
2026-07-30 14:34:02 +0300

    feat(allergies): Add delete command and register it in DI

---
+++ Hospital-ERP-Backend.Application/Features/Allergies/Commands/DeleteAllergy/DeleteAllergyService.cs
@@ -0,0 +1,68 @@
+﻿using FluentValidation;
+using Hospital_ERP_Backend.Domain.Entities;
+using Hospital_ERP_Backend.Domain.Interfaces.Base;
+using MediatR;
+
+namespace Hospital_ERP_Backend.Application.Features.Allergies.Commands.DeleteAllergy
+{
+    public class DeleteAllergyService :
+        IRequestHandler<DeleteAllergyRequest, bool>
+    {
+        private readonly IBaseCommandRepository<Allergy> _repository;
+
+        private readonly IBaseQueryRepository<Allergy> _queryRepository;
+
+        private readonly IValidator<DeleteAllergyRequest> _validator;
+
+        public DeleteAllergyService(
+            IBaseCommandRepository<Allergy> repository,
+            IBaseQueryRepository<Allergy> queryRepository,
+            IValidator<DeleteAllergyRequest> validator)
+        {
+            _repository = repository;
+            _queryRepository = queryRepository;
+            _validator = validator;
+        }
+
+        public async Task<bool> Handle(
+            DeleteAllergyRequest request,
+            CancellationToken cancellationToken)
+        {
+            return await DeleteAllergyAsync(request);
+        }
+
+        private async Task<bool> DeleteAllergyAsync(
+            DeleteAllergyRequest request)
+        {
+            var validationResult =
+                await _validator.ValidateAsync(request);
+
+            if (!validationResult.IsValid)
+            {
+                throw new ArgumentException(
+                    string.Join(", ",
+                    validationResult.Errors.Select(x => x.ErrorMessage)));
+            }
+
+            Allergy? allergy =
+                await _queryRepository.GetAsync(request.Id);
+
+            if (allergy == null)
+            {
+                throw new KeyNotFoundException(
+                    $"Allergy with Id {request.Id} not found.");
+            }
+
+            bool isDeleted =
+                await _repository.DeleteAsync(allergy.Id);
+
+            if (!isDeleted)
+            {
+                throw new InvalidOperationException(
+                    $"Failed to delete Allergy with Id {request.Id}.");
+            }
+
+            return isDeleted;
+        }
+    }
+}
--- Hospital-ERP-Backend.Application/Features/Allergies/Extensions/AllergyServiceExtensions.cs
+++ Hospital-ERP-Backend.Application/Features/Allergies/Extensions/AllergyServiceExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using Hospital_ERP_Backend.Application.Features.Allergies.Commands.CreateAllergy;
+using Hospital_ERP_Backend.Application.Features.Allergies.Commands.DeleteAllergy;
 using Hospital_ERP_Backend.Application.Features.Allergies.Commands.UpdateAllergy;
 using Hospital_ERP_Backend.Application.Features.Allergies.Queries.GetAllAllergies;
 using Hospital_ERP_Backend.Application.Features.Allergies.Queries.GetAllergy;
@@ -15,11 +16,13 @@ namespace Hospital_ERP_Backend.Application.Features.Allergies.Extensions
             service.AddScoped<IValidator<GetAllAllergiesRequest>, GetAllAllergiesValidator>();
             service.AddScoped<IValidator<CreateAllergyRequest>, CreateAllergyValidator>();
             service.AddScoped<IValidator<UpdateAllergyRequest>, UpdateAllergyValidator>();
+            service.AddScoped<IValidator<DeleteAllergyRequest>, DeleteAllergyValidator>();
 
             service.AddScoped<GetAllergyService>();
             service.AddScoped<GetAllAllergiesService>();
             service.AddScoped<CreateAllergyService>();
             service.AddScoped<UpdateAllergyService>();
+            service.AddScoped<DeleteAllergyService>();
 
             return service;
         }
