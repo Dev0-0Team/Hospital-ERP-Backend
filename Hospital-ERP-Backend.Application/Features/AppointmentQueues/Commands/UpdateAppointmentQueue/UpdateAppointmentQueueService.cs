eb289a72a144541021538813634bfd22497f50c7
ali-mousa <alimoussajob@gmail.com>
2026-08-01 17:13:08 +0300

    test(appointment queue): fix logic update appointment queue service

--- Hospital-ERP-Backend.Application/Features/AppointmentQueues/Commands/UpdateAppointmentQueue/UpdateAppointmentQueueService.cs
+++ Hospital-ERP-Backend.Application/Features/AppointmentQueues/Commands/UpdateAppointmentQueue/UpdateAppointmentQueueService.cs
@@ -9,13 +9,12 @@ namespace Hospital_ERP_Backend.Application.Features.AppointmentQueues.Commands.U
     {
         private readonly IValidator<UpdateAppointmentQueueRequest> _validator;
         private readonly IBaseCommandRepository<AppointmentQueue> _iAppointmentQueue;
-        private readonly IBaseQueryRepository<AppointmentQueue> _iAppointmentQueueQuery;
-
-        public UpdateAppointmentQueueService(IValidator<UpdateAppointmentQueueRequest> validator, IBaseCommandRepository<AppointmentQueue> iAppointmentQueue, IBaseQueryRepository<AppointmentQueue> iAppointmentQueueQuery)
+        private readonly IBaseCommandRepository<Appointment> _iAppointment;
+        public UpdateAppointmentQueueService(IValidator<UpdateAppointmentQueueRequest> validator, IBaseCommandRepository<AppointmentQueue> iAppointmentQueue, IBaseCommandRepository<Appointment> iAppointment)
         {
             _validator = validator;
             _iAppointmentQueue = iAppointmentQueue;
-            _iAppointmentQueueQuery = iAppointmentQueueQuery;
+            _iAppointment = iAppointment;
         }
 
         public async Task<UpdateAppointmentQueueResponse> Handle(UpdateAppointmentQueueRequest request, CancellationToken cancellationToken)
@@ -31,7 +30,13 @@ namespace Hospital_ERP_Backend.Application.Features.AppointmentQueues.Commands.U
                 throw new ArgumentException($"Invalid request: {string.Join(", ", validationResult.Errors.Select(e => e.ErrorMessage))}");
             }
 
-            AppointmentQueue? existingAppointmentQueue = await _iAppointmentQueueQuery.GetAsync(request.Id);
+            bool isAppointmentExist = await _iAppointment.IsExistAsync(request.AppointmentId);
+            if (!isAppointmentExist)
+            {
+                throw new KeyNotFoundException($"Appointment with Id {request.AppointmentId} not found.");
+            }
+
+            AppointmentQueue? existingAppointmentQueue = await _iAppointmentQueue.FindAsync(request.Id);
             if (existingAppointmentQueue == null)
             {
                 throw new KeyNotFoundException($"Appointment queue with Id {request.Id} not found.");
@@ -40,7 +45,7 @@ namespace Hospital_ERP_Backend.Application.Features.AppointmentQueues.Commands.U
             existingAppointmentQueue.AppointmentId = request.AppointmentId;
             existingAppointmentQueue.QueueNumber = request.QueueNumber;
             existingAppointmentQueue.EstimatedTime = request.EstimatedTime;
-            existingAppointmentQueue.Status = request.Status;
+            existingAppointmentQueue.Status = request.Status.ToString();
             existingAppointmentQueue.UpdatedAt = DateTime.Now;
 
             AppointmentQueue? result = await _iAppointmentQueue.UpdateAsync(existingAppointmentQueue);
