b113d54ba9e556830c3930b303615178a530ad80
Mohamed khelifi <khelifim440@gmail.com>
2026-08-03 10:45:37 +0100

    feat(EmergencyCases) : create queries/commands repositories

---
+++ Hospital-ERP-Backend.Infrastructure/Repositories/Queries/EmergencyCasesQueryRepository.cs
@@ -0,0 +1,21 @@
+﻿using Hospital_ERP_Backend.Domain.Entities;
+using Hospital_ERP_Backend.Infrastructure.Repositories.Queries.Base;
+using Hospital_ERP_Backend.Infrastructure.Setting;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital_ERP_Backend.Infrastructure.Repositories.Queries
+{
+    public class EmergencyCasesQueryRepository : BaseQueryRepository<EmergencyCase>
+    {
+        protected override string GetAllSpName => "EmergencyCases.SP_GetAllEmergencyCases";
+
+        protected override string GetByIdSpName => "SP_GetEmergencyCaseById";
+
+       public EmergencyCasesQueryRepository(IOptions<MySetting> settings) : base(settings) { }
+    }
+}
