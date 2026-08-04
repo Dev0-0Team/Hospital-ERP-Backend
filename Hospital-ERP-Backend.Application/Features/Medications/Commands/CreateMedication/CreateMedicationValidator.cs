0b828ae2da19d01691133be80d0d6948f5b4d887
ali-mousa <alimoussajob@gmail.com>
2026-08-04 16:35:19 +0300

    test(medication): fix logic update medication service

--- Hospital-ERP-Backend.Application/Features/Medications/Commands/CreateMedication/CreateMedicationValidator.cs
+++ Hospital-ERP-Backend.Application/Features/Medications/Commands/CreateMedication/CreateMedicationValidator.cs
@@ -8,14 +8,14 @@ namespace Hospital_ERP_Backend.Application.Features.Medications.Commands.CreateM
         {
             RuleFor(x => x.Name)
                 .NotEmpty().WithMessage("Medication name is required.")
-                .MaximumLength(255);
+                .MaximumLength(255).WithMessage("Medication name cannot exceed 255 characters.");
 
             RuleFor(x => x.DosageForm)
                 .NotEmpty().WithMessage("Dosage form is required.")
-                .MaximumLength(50);
+                .MaximumLength(50).WithMessage("Dosage form cannot exceed 50 characters.");
 
             RuleFor(x => x.Manufacturer)
-                .MaximumLength(150).WithMessage("Manufacturer name is too long.");
+                .MaximumLength(150).WithMessage("Manufacturer cannot exceed 150 characters.");
         }
     }
 }
--- Hospital-ERP-Backend.Application/Features/Medications/Commands/UpdateMedication/UpdateMedicationService.cs
+++ Hospital-ERP-Backend.Application/Features/Medications/Commands/UpdateMedication/UpdateMedicationService.cs
@@ -9,12 +9,10 @@ namespace Hospital_ERP_Backend.Application.Features.Medications.Commands.UpdateM
     {
         private readonly IValidator<UpdateMedicationRequest> _validator;
         private readonly IBaseCommandRepository<Medication> _medicationRepository;
-        private readonly IBaseQueryRepository<Medication> _medicationQueryRepository;
-        public UpdateMedicationService(IValidator<UpdateMedicationRequest> validator, IBaseCommandRepository<Medication> medicationRepository, IBaseQueryRepository<Medication> medicationQueryRepository)
+        public UpdateMedicationService(IValidator<UpdateMedicationRequest> validator, IBaseCommandRepository<Medication> medicationRepository)
         {
             _validator = validator;
             _medicationRepository = medicationRepository;
-            _medicationQueryRepository = medicationQueryRepository;
         }
         public async Task<UpdateMedicationResponse> Handle(UpdateMedicationRequest request, CancellationToken cancellationToken)
         {
@@ -28,7 +26,7 @@ namespace Hospital_ERP_Backend.Application.Features.Medications.Commands.UpdateM
             {
                 throw new ArgumentException($"Invalid request: {string.Join(", ", validationResult.Errors.Select(e => e.ErrorMessage))}");
             }
-            var medication = await _medicationQueryRepository.GetAsync(request.Id);
+            var medication = await _medicationRepository.FindAsync(request.Id);
 
             if (medication == null)
             {
