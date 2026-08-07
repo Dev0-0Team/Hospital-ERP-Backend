0680c3040ebdac7ecad3c1ee1bef5a37d5b1cfca
Abdelrahman Owais <abdoo.owais@gmail.com>
2026-08-07 15:25:49 +0300

    feat(permissions bits enums): Add radiology permissions enum

---
+++ Hospital-ERP-Backend.Domain/Enums/PermissionBits/RadiologyPermissions.cs
@@ -0,0 +1,30 @@
+﻿namespace Hospital_ERP_Backend.Domain.Enums.PermissionBits
+{
+    [Flags]
+    public enum RadiologyPermissions : ulong
+    {
+        None = 0,
+
+        RadiologyOrdersCreate = 1UL << 0,
+        RadiologyOrdersRead = 1UL << 1,
+        RadiologyOrdersUpdate = 1UL << 2,
+        RadiologyOrdersDelete = 1UL << 3,
+
+        RadiologyOrdersManage = RadiologyOrdersCreate | RadiologyOrdersRead | RadiologyOrdersUpdate | RadiologyOrdersDelete,
+
+        RadiologyReportsCreate = 1UL << 4,
+        RadiologyReportsRead = 1UL << 5,
+        RadiologyReportsUpdate = 1UL << 6,
+        RadiologyReportsDelete = 1UL << 7,
+
+        RadiologyReportsManage = RadiologyReportsCreate | RadiologyReportsRead | RadiologyReportsUpdate | RadiologyReportsDelete,
+
+        RadiologyImagesCreate = 1UL << 8,
+        RadiologyImagesRead = 1UL << 9,
+        RadiologyImagesUpdate = 1UL << 10,
+        RadiologyImagesDelete = 1UL << 11,
+
+        RadiologyImagesManage = RadiologyImagesCreate | RadiologyImagesRead | RadiologyImagesUpdate | RadiologyImagesDelete,
+
+    }
+}
