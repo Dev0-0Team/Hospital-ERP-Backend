380b6666099e534ec8f378b0691a990a6f644b81
Abdelrahman Owais <abdoo.owais@gmail.com>
2026-08-07 15:23:51 +0300

    feat(permissions bits enums): Add laboratory permissions enum

---
+++ Hospital-ERP-Backend.Domain/Enums/PermissionBits/LaboratoryPermissions.cs
@@ -0,0 +1,23 @@
+﻿namespace Hospital_ERP_Backend.Domain.Enums.PermissionBits
+{
+    [Flags]
+    public enum LaboratoryPermissions : ulong
+    {
+        None = 0,
+
+        LabTestsCreate = 1UL << 0,
+        LabTestsRead = 1UL << 1,
+        LabTestsUpdate = 1UL << 2,
+        LabTestsDelete = 1UL << 3,
+
+        LabTestsManage = LabTestsCreate | LabTestsRead | LabTestsUpdate | LabTestsDelete,
+
+        LabOrdersCreate = 1UL << 4,
+        LabOrdersRead = 1UL << 5,
+        LabOrdersUpdate = 1UL << 6,
+        LabOrdersDelete = 1UL << 7,
+
+        LabOrdersManage = LabOrdersCreate | LabOrdersRead | LabOrdersUpdate | LabOrdersDelete,
+
+    }
+}
