100010ee6df2e1da16e58b4da5fab66f0dde9574
Abdelrahman Owais <abdoo.owais@gmail.com>
2026-08-10 13:01:10 +0300

    feat(auth): Add register controller

---
+++ Hopital-ERP-Backend.API/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using Hospital_ERP_Backend.Application.Features.Authentication.Commands.Register;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hospital_ERP_Backend.API.Controllers
+{
+    [Route("api/Auth")]
+    [ApiController]
+    public class AuthController : BaseController
+    {
+        private readonly ISender _sender;
+
+        public AuthController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpPost("Register", Name = "RegisterAsync")]
+        public async Task<ActionResult<ApiResponse<RegisterResponse>>> RegisterAsync([FromBody] RegisterRequest request)
+        {
+            var response = await _sender.Send(request);
+
+            return CreateResponse<RegisterResponse>(
+                response,
+                StatusCodes.Status201Created,
+                "User Registered Successfully!");
+        }
+    }
+}
