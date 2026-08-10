e4da4cd48e0aa5c98ee4a3faca56d802b32934b9
Abdelrahman Owais <abdoo.owais@gmail.com>
2026-08-10 12:21:35 +0300

    feat(user): Add GetByEmail interface

--- Hospital-ERP-Backend.Domain/Interfaces/Base/IBaseQueryRepository.cs
+++ Hospital-ERP-Backend.Domain/Interfaces/Base/IBaseQueryRepository.cs
@@ -5,5 +5,8 @@ namespace Hospital_ERP_Backend.Domain.Interfaces.Base
     {
         Task<IEnumerable<Entity>> GetAllAsync(int page);
         Task<Entity?> GetAsync(int ID);
+
+        Task<Entity?> GetUserByEmailAsync(string email);
+        Task<bool> IsEmailExists(string email);
     }
 }
