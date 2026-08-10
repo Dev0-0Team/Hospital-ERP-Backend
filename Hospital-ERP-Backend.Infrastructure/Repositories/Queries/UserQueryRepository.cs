d33c8eb90959a1be65ce46ada649fbd342183e72
Abdelrahman Owais <abdoo.owais@gmail.com>
2026-08-10 11:51:02 +0300

    feat(user): Add GetByEmail query repo

--- Hospital-ERP-Backend.Infrastructure/Repositories/Queries/UserQueryRepository.cs
+++ Hospital-ERP-Backend.Infrastructure/Repositories/Queries/UserQueryRepository.cs
@@ -1,7 +1,9 @@
-﻿using Hospital_ERP_Backend.Domain.Entities;
+﻿using Dapper;
+using Hospital_ERP_Backend.Domain.Entities;
 using Hospital_ERP_Backend.Infrastructure.Repositories.Queries.Base;
 using Hospital_ERP_Backend.Infrastructure.Setting;
 using Microsoft.Extensions.Options;
+using System.Data;
 
 
 namespace Hospital_ERP_Backend.Infrastructure.Repositories.Queries
@@ -10,6 +12,14 @@ namespace Hospital_ERP_Backend.Infrastructure.Repositories.Queries
     {
         protected override string GetAllSpName => "users.SP_GetAllUsers";
         protected override string GetByIdSpName => "users.SP_GetUserById";
+
+        public async Task<User?> GetUserByEmailAsync(string email)
+        {
+
+            return await _connection.QueryFirstOrDefaultAsync<User>("users.SP_GetUserByEmail",
+                new { email = email },
+                commandType: CommandType.StoredProcedure);
+        }
 
         public UserQueryRepository(IOptions<MySetting> setting) : base(setting) { }
 
