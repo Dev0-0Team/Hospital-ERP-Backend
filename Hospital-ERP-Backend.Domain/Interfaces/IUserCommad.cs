576cf6585657a19baa03542ad9960c79483faccc
Abdelrahman Owais <abdoo.owais@gmail.com>
2026-08-14 18:02:41 +0300

    feat(user): Add user command interface in domin

---
+++ Hospital-ERP-Backend.Domain/Interfaces/IUserCommad.cs
@@ -0,0 +1,9 @@
+﻿using Hospital_ERP_Backend.Domain.Entities;
+
+namespace Hospital_ERP_Backend.Domain.Interfaces
+{
+    public interface IUserCommand
+    {
+        Task AddAsync(User user);
+    }
+}
