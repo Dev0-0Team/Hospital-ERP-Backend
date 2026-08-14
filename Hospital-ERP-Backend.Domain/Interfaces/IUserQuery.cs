e6ea16b0b78d1df309ed56ed7e0300bc0c4faac3
ali-mousa <alimoussajob@gmail.com>
2026-08-14 17:46:13 +0300

    refactor(user): delete template

--- Hospital-ERP-Backend.Domain/Interfaces/IUserQuery.cs
+++ Hospital-ERP-Backend.Domain/Interfaces/IUserQuery.cs
@@ -1,8 +1,9 @@
-﻿using Hospital_ERP_Backend.Domain.Interfaces.Base;
+﻿using Hospital_ERP_Backend.Domain.Entities;
+using Hospital_ERP_Backend.Domain.Interfaces.Base;
 
 namespace Hospital_ERP_Backend.Domain.Interfaces
 {
-    public interface IUserQuery<User> : IBaseQueryRepository<User>
+    public interface IUserQuery : IBaseQueryRepository<User>
     {
         Task<User?> GetUserByEmailAsync(string email);
 
