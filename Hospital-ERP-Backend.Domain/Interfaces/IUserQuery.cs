fcd10c4b65837d6b4833361f2a5af184cf6e4580
Abdelrahman Owais <abdoo.owais@gmail.com>
2026-08-14 16:45:40 +0300

    feat(user): Add user interface in domin

---
+++ Hospital-ERP-Backend.Domain/Interfaces/IUserQuery.cs
@@ -0,0 +1,12 @@
+﻿using Hospital_ERP_Backend.Domain.Entities;
+using Hospital_ERP_Backend.Domain.Interfaces.Base;
+
+namespace Hospital_ERP_Backend.Domain.Interfaces
+{
+    public interface IUserQuery : IBaseQueryRepository<User>
+    {
+        Task<User?> GetUserByEmailAsync(string email);
+
+        Task<bool> IsEmailExistsAsync(string email);
+    }
+}
