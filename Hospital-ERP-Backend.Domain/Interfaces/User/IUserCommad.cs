1735a4adf1141213d351f5850c7bd5fbedfc09fa
ali-mousa <alimoussajob@gmail.com>
2026-08-16 19:18:25 +0300

    refactor(user): fix bugs and refactor code

---
+++ Hospital-ERP-Backend.Domain/Interfaces/User/IUserCommad.cs
@@ -0,0 +1,8 @@
+﻿using Hospital_ERP_Backend.Domain.Interfaces.Base;
+
+namespace Hospital_ERP_Backend.Domain.Interfaces.User
+{
+    public interface IUserCommandRepository : IBaseCommandRepository<Hospital_ERP_Backend.Domain.Entities.User>
+    {
+    }
+}
