7a0083c5a0b8560a0b5a4072a9eb3fdea0318096
Abdelrahman Owais <abdoo.owais@gmail.com>
2026-08-27 19:22:44 +0300

    feat(logout): Add logout and register it in DI

--- Hospital-ERP-Backend.Application/Features/Authentication/Commands/Logout/LogoutRequest.cs
+++ Hospital-ERP-Backend.Application/Features/Authentication/Commands/Logout/LogoutRequest.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
 
-namespace Hospital_ERP_Backend.Application.Features.Authentication.Commands.Logout
+namespace Hospital_ERP_Backend.Application.Features.Authentication.Commands.Logout;
+
+public sealed record LogoutRequest : IRequest<bool>
 {
-    internal class LogoutRequest
-    {
-    }
+    public string RefreshToken { get; set; } = null!;
 }
