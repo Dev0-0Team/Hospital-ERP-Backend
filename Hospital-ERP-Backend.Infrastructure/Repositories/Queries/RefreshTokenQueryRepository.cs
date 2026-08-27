706c5eb26679a5e154c28953f1024aaae58a7a9c
Abdelrahman Owais <abdoo.owais@gmail.com>
2026-08-27 18:46:05 +0300

    feat(refresh-token): Add GetHashed in query repo and register it in DI

---
+++ Hospital-ERP-Backend.Infrastructure/Repositories/Queries/RefreshTokenQueryRepository.cs
@@ -0,0 +1,27 @@
+﻿using Hospital_ERP_Backend.Domain.Entities;
+using Hospital_ERP_Backend.Infrastructure.Data;
+using Hospital_ERP_Backend.Infrastructure.Repositories.Queries.Base;
+using Hospital_ERP_Backend.Infrastructure.Setting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Hospital_ERP_Backend.Infrastructure.Repositories.Queries
+{
+    internal class RefreshTokenQueryRepository : BaseQueryRepository<RefreshToken>, IRefreshTokenQueryRepository
+    {
+        private readonly HospitalDbContext _context;
+
+        protected override string GetAllSpName => "refresh_tokens.SP_GetAllRefreshTokens ";
+        protected override string GetByIdSpName => "refresh_tokens.SP_GetRefreshTokenById";
+
+        public RefreshTokenQueryRepository(IOptions<MySetting> options, HospitalDbContext context) : base(options)
+        {
+            _context = context;
+        }
+
+        public async Task<RefreshToken?> GetByHashAsync(string hash)
+        {
+            return await _context.RefreshTokens.FirstOrDefaultAsync(x => x.TokenHash == hash);
+        }
+    }
+}
